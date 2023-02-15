a508030669fc2b55b27867330ebb94c373156f47
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-15 12:47:11 +0100

    Update GenericFunctions.py

--- GenericFunctions.py
+++ GenericFunctions.py
@@ -1,1 +1,122 @@
+import os
+import subprocess
+import zipfile
+
+class GenericFunctions:
+    def InstallationType(self):
+        # Your code here
+        pass
+
+    def CheckWorkspace(self, workspace):
+        # Your code here
+        pass
+
+    def HBPassword(self, password):
+        # Your code here
+        pass
+
+    def HOPassword(self, password):
+        # Your code here
+        pass
+
+    def RepoUpdateType(self, update_type):
+        # Your code here
+        pass
+
+    def RebootAutomatically(self, enable):
+        # Your code here
+        pass
+
+    def InstallMavenCLI(self):
+        # Your code here
+        pass
+
+    def InstallJRE(self):
+        # Your code here
+        pass
+
+    def InstallMavenPlugin(self, plugin_name):
+        # Your code here
+        pass
+
+    def CheckLDAPGroup(self, group_name):
+        # Your code here
+        pass
+
+    def checkPathExists(self, path):
+        # Your code here
+        pass
+
+    def checkCommandWorking(self, command):
+        # Your code here
+        pass
+
+    def checkCVSConnection(self, url):
+        # Your code here
+        pass
+
+    def checkGitConnection(self, url):
+        # Your code here
+        pass
+
+    def CheckoutEAIDefaults(self, branch_name):
+        # Your code here
+        pass
+
+    def CheckoutETLDefaults(self, branch_name):
+        # Your code here
+        pass
+
+    def getDataFromGit(self, url, branch_name, local_dir):
+        # Your code here
+        pass
+
+    def getDataFromCVS(self, url, local_dir):
+        # Your code here
+        pass
+
+    def createLocalPropertyFiles(self):
+        # Your code here
+        pass
+
+    def getEAIDeveloperPorts(self):
+        # Your code here
+        pass
+
+    def replaceStringInFile(self, file_path, old_string, new_string):
+        # Your code here
+        pass
+
+    def copyFiles(self, src_path, dst_path):
+        # Your code here
+        pass
+
+    def getEncryptedPassword(self, password):
+        # Your code here
+        pass
+
+    def checkIfExecutableRunning(self, executable_name):
+        # Your code here
+        pass
+
+    def extractZipFile(self, zip_path, extract_path):
+        # Your code here
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+
+    def installToolkitPluginFromLocal(self, local_path):
+        # Your code here
+        pass
+
+    def importDefaultProjectsEAI(self):
+        # Your code here
+        pass
+
+    def installEclipsePlugin(self, plugin_url):
+        # Your code here
+        pass
+
+    def importProjectUsingBarFile(self, bar_file_path):
+        # Your code here
+        pass
 
