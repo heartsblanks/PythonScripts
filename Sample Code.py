fe555acdeebeb4cb5bd8033cafd89a06129eb147
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-15 08:40:46 +0100

    Update Sample Code.py

--- Sample Code.py
+++ Sample Code.py
@@ -3,8 +3,40 @@ from tkinter import ttk
 from tkinter import messagebox
 
 def dummy_function():
-    # placeholder for the function to be called when IIB or ACE is clicked
-    pass
+    # create a new window for installation setup options
+    setup_window = tk.Toplevel()
+    setup_window.title("Installation Setup")
+
+    # create labels and radio buttons for unattended installation, repository update type, and reboot
+    ui_label = ttk.Label(setup_window, text="Unattended Installation:")
+    ui_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+    ui_var = tk.StringVar()
+    ui_yes = ttk.Radiobutton(setup_window, text="Yes", value="yes", variable=ui_var)
+    ui_yes.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+    ui_no = ttk.Radiobutton(setup_window, text="No", value="no", variable=ui_var)
+    ui_no.grid(row=0, column=2, padx=5, pady=5, sticky="w")
+
+    rut_label = ttk.Label(setup_window, text="Repository Update Type:")
+    rut_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+    rut_var = tk.StringVar()
+    rut_update = ttk.Radiobutton(setup_window, text="Update", value="update", variable=rut_var)
+    rut_update.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+    rut_replace = ttk.Radiobutton(setup_window, text="Replace", value="replace", variable=rut_var)
+    rut_replace.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+
+    ra_label = ttk.Label(setup_window, text="Reboot Automatically:")
+    ra_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+    ra_var = tk.StringVar()
+    ra_yes = ttk.Radiobutton(setup_window, text="Yes", value="yes", variable=ra_var)
+    ra_yes.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+    ra_no = ttk.Radiobutton(setup_window, text="No", value="no", variable=ra_var)
+    ra_no.grid(row=2, column=2, padx=5, pady=5, sticky="w")
+
+    # create a submit button to call the installation_setup function
+    submit_button = ttk.Button(setup_window, text="Submit",
+                               command=lambda: installation_setup(ui_var.get(), rut_var.get(), ra_var.get()))
+    submit_button.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
+
 
 def encrypt_password(hb_password, ho_password):
     # placeholder for the encryption function
