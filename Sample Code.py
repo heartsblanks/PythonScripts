53b0f2ab197fb87d85d1380439106b22b115b73b
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-15 08:26:13 +0100

    Create Sample Code.py

---
+++ Sample Code.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+def dummy_function():
+    # placeholder for the function to be called when IIB or ACE is clicked
+    pass
+
+def encrypt_password(hb_password, ho_password):
+    # placeholder for the encryption function
+    pass
+
+def password_update_window():
+    # create a new window for password update
+    password_window = tk.Toplevel()
+    password_window.title("Password Update")
+    
+    # create labels and entry widgets for HB password and HO password
+    hb_label = ttk.Label(password_window, text="HB Password:")
+    hb_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+    hb_entry = ttk.Entry(password_window, show="*")
+    hb_entry.grid(row=0, column=1, padx=5, pady=5)
+    
+    ho_label = ttk.Label(password_window, text="HO Password:")
+    ho_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+    ho_entry = ttk.Entry(password_window, show="*")
+    ho_entry.grid(row=1, column=1, padx=5, pady=5)
+    
+    # create a submit button to encrypt and store the passwords
+    submit_button = ttk.Button(password_window, text="Submit",
+                               command=lambda: encrypt_password(hb_entry.get(), ho_entry.get()))
+    submit_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+def eai_button_click():
+    # create a new window for EAI options
+    eai_window = tk.Toplevel()
+    eai_window.title("EAI Options")
+    
+    # create radio buttons for IIB, ACE, and Password Update
+    iib_button = ttk.Radiobutton(eai_window, text="IIB", value="iib",
+                                 command=dummy_function)
+    iib_button.pack(padx=5, pady=5, anchor="w")
+    
+    ace_button = ttk.Radiobutton(eai_window, text="ACE", value="ace",
+                                 command=dummy_function)
+    ace_button.pack(padx=5, pady=5, anchor="w")
+    
+    password_button = ttk.Radiobutton(eai_window, text="Password Update",
+                                       value="password", command=password_update_window)
+    password_button.pack(padx=5, pady=5, anchor="w")
+
+def etl_button_click():
+    # placeholder for the function to be called when ETL is clicked
+    pass
+
+# create the main window with radio buttons for EAI and ETL
+root = tk.Tk()
+root.title("Options")
+
+eai_button = ttk.Radiobutton(root, text="EAI", value="eai",
+                             command=eai_button_click)
+eai_button.pack(padx=10, pady=10)
+
+etl_button = ttk.Radiobutton(root, text="ETL", value="etl",
+                             command=etl_button_click)
+etl_button.pack(padx=10, pady=10)
+
+root.mainloop()
