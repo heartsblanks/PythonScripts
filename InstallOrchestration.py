03f42accf2cbc7d65f3aae0ce1fc8cc72134f290
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-16 11:03:24 +0100

    Update InstallOrchestration.py

--- InstallOrchestration.py
+++ InstallOrchestration.py
@@ -28,12 +28,13 @@ class InstallOrchestration:
 
     def create_system_button(self, system_name, options):
         # Create button
-        button = ttk.Button(self.master, text=system_name, command=lambda: self.displaySystemOptions(options), style="SystemButton.TButton")
+        button = ttk.Button(self.master, text=system_name, command=lambda: self.displaySystemOptions(system_name, options), style="SystemButton.TButton")
         button.pack(fill="x", padx=10, pady=10)
 
-    def displaySystemOptions(self, options):
+    def displaySystemOptions(self, system_type, options):
         # Create new top level window
         self.system_options_window = tk.Toplevel(self.master)
+        self.system_type = system_type
         system_type = self.system_options_window.title(" ".join(options) + " Options")
 
         # Set up custom styles for this window
@@ -48,42 +49,7 @@ class InstallOrchestration:
         password_update_button = ttk.Button(self.system_options_window, text="Password update", command=self.passwordUpdate, style="SystemButton.TButton")
         password_update_button.pack(fill="x", padx=10, pady=5)
 
-    def setup_system_options_styles(self):
-        self.system_options_window.option_add("*Button.Background", "#4d4d4d")
-        self.system_options_window.option_add("*Button.Foreground", "white")
-        self.system_options_window.option_add("*Button.activeBackground", "#808080")
-        self.system_options_window.option_add("*Button.activeForeground", "white")
-        self.system_options_window.option_add("*Button.highlightThickness", 0)
-        self.system_options_window.option_add("*Label.Background", "#d9d9d9")
-        self.system_options_window.option_add("*Label.Foreground", "#4d4d4d")
-    def displaySystemSetupOptions(self, system_type, install_type):
-        # Create new top level window
-        self.system_setup_options_window = tk.Toplevel(self.master)
-        self.system_setup_options_window.title(f"{system_type} {install_type} Setup")
-
-        # Set up custom styles for this window
-        self.setup_system_setup_options_styles()
-
-        # Create labels and options
-        self.installation_type_var = self.create_label_and_options("Unattended installation", ["Yes", "No"])
-        self.repo_update_var = self.create_label_and_options("Repository", ["Update", "Replace"])
-        self.hb_password_entry = self.create_label_and_entry("HB Password")
-        if install_type in ["IIB10", "ACE12"]:
-            self.ho_password_entry = self.create_label_and_entry("HO Password")
-            self.auto_reboot_var = self.create_label_and_options("Reboot Automatically", ["Yes", "No"])
-        
-            
-        
-
-        # Create submit button
-        submit_button = ttk.Button(self.system_setup_options_window, text="Submit", command=lambda: self.performInstallation(system_type, install_type), style="SubmitButton.TButton")
-        submit_button.pack(pady=10)
-
-        # Create quit button
-        quit_button = ttk.Button(self.system_setup_options_window, text="Quit", command=self.system_setup_options_window.destroy, style="QuitButton.TButton")
-        quit_button.pack(pady=10)
-
-
+ 
     def setup_system_setup_options_styles(self):
         self.system_setup_options_window.option_add("*Button.Background", "#4d4d4d")
         self.system_setup_options_window.option_add("*Button.Foreground", "white")
