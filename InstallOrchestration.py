3360f38856225d8c260f41ddb9beae3425275958
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-16 01:24:30 +0100

    Update InstallOrchestration.py

--- InstallOrchestration.py
+++ InstallOrchestration.py
@@ -1,55 +1,477 @@
 import tkinter as tk
+from tkinter import messagebox
 
-class InstallOrchestration:
+class ApplicationInstaller:
+    """Class to create a GUI for installing and configuring applications"""
+    COLOR_PRIMARY = "#1E90FF"
+    COLOR_SECONDARY = "white"
+    FONT_SMALL = ("Arial", 10)
+    FONT_MEDIUM = ("Arial", 12, "bold")
+    FONT_LARGE = ("Arial", 14, "bold")
+
     def __init__(self):
+        """Initialize the GUI"""
         self.root = tk.Tk()
-        self.root.geometry('300x150')
-        self.root.title('Install Orchestration')
-        
-        self.submit_button = tk.Button(self.root, text='Submit', command=self.select_option)
-        self.submit_button.pack(side='left', padx=10, pady=10)
-        
-        self.quit_button = tk.Button(self.root, text='Quit', command=self.root.destroy)
-        self.quit_button.pack(side='right', padx=10, pady=10)
-        
-        self.top_level_window = tk.Toplevel(self.root)
-        self.top_level_window.title('Select Option')
-        self.top_level_window.geometry('300x100')
-        
-        self.eai_button = tk.Button(self.top_level_window, text='EAI', command=self.select_eai)
-        self.eai_button.pack(side='left', padx=10, pady=10)
-        
-        self.etl_button = tk.Button(self.top_level_window, text='ETL', command=self.select_etl)
-        self.etl_button.pack(side='right', padx=10, pady=10)
-        
-        self.root.mainloop()
-    
-    def select_option(self):
-        pass
-    
-    def select_eai(self):
-        self.eai_window = tk.Toplevel(self.root)
-        self.eai_window.title('Select EAI Option')
-        self.eai_window.geometry('300x100')
-        
-        self.iib_button = tk.Button(self.eai_window, text='IIB10', command=self.get_eai_options)
-        self.iib_button.pack(side='left', padx=10, pady=10)
-        
-        self.ace_button = tk.Button(self.eai_window, text='ACE12', command=self.get_eai_options)
-        self.ace_button.pack(side='right', padx=10, pady=10)
-        
-        self.password_button = tk.Button(self.eai_window, text='Password Update', command=self.password_update)
-        self.password_button.pack(side='bottom', padx=10, pady=10)
-    
-    def select_etl(self):
-        pass
-    
-    def get_eai_options(self):
-        pass
-    
-    def password_update(self):
-        pass
+        self.root.title("Application Installer")
+        self.root.geometry("400x400")
 
-if __name__ == '__main__':
-    InstallOrchestration()
- 
+        # Create buttons for EAI and ETL
+        self.eai_button = tk.Button(
+            self.root,
+            text="EAI",
+            command=self.display_eai_options,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.eai_button.grid(row=0, column=0, pady=10)
+
+        self.etl_button = tk.Button(
+            self.root,
+            text="ETL",
+            command=self.display_etl_options,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.etl_button.grid(row=1, column=0, pady=10)
+    def display_eai_options(self):
+        """Display options for installing and configuring EAI"""
+        # Create new top level window for EAI options
+        self.eai_options_window = tk.Toplevel(self.root)
+        self.eai_options_window.title("EAI Options")
+        self.eai_options_window.geometry("400x400")
+
+        # Create buttons for IIB10, ACE12, and Password update
+        self.iib10_button = tk.Button(
+            self.eai_options_window,
+            text="IIB 10",
+            command=self.display_eai_setup_options,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.iib10_button.grid(row=0, column=0, pady=10)
+
+        self.ace12_button = tk.Button(
+            self.eai_options_window,
+            text="ACE 12",
+            command=self.display_eai_setup_options,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.ace12_button.grid(row=1, column=0, pady=10)
+
+        self.password_update_button = tk.Button(
+            self.eai_options_window,
+            text="Password Update",
+            command=self.display_password_update,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.password_update_button.grid(row=2, column=0, pady=10)
+    def display_eai_setup_options(self):
+        """Display options for setting up EAI"""
+        # Create new top level window for EAI setup options
+        self.eai_setup_window = tk.Toplevel(self.eai_options_window)
+        self.eai_setup_window.title("EAI Setup Options")
+        self.eai_setup_window.geometry("400x400")
+
+        # Create label and radio buttons for installation type
+        self.installation_type_label = tk.Label(
+            self.eai_setup_window,
+            text="Unattended Installation:",
+            font=self.FONT_MEDIUM,
+        )
+        self.installation_type_label.grid(row=0, column=0, pady=10)
+
+        self.installation_type_var = tk.StringVar()
+        self.installation_type_var.set("Yes")
+
+        self.installation_type_yes = tk.Radiobutton(
+            self.eai_setup_window,
+            text="Yes",
+            variable=self.installation_type_var,
+            value="Yes",
+            font=self.FONT_SMALL,
+        )
+        self.installation_type_yes.grid(row=1, column=0, pady=10)
+
+        self.installation_type_no = tk.Radiobutton(
+            self.eai_setup_window,
+            text="No",
+            variable=self.installation_type_var,
+            value="No",
+            font=self.FONT_SMALL,
+        )
+        self.installation_type_no.grid(row=2, column=0, pady=10)
+        # Create label and radio buttons for automatic reboot
+        self.auto_reboot_label = tk.Label(
+            self.eai_setup_window,
+            text="Reboot Automatically:",
+            font=self.FONT_MEDIUM,
+        )
+        self.auto_reboot_label.grid(row=3, column=0, pady=10)
+
+        self.auto_reboot_var = tk.StringVar()
+        self.auto_reboot_var.set("Yes")
+
+        self.auto_reboot_yes = tk.Radiobutton(
+            self.eai_setup_window,
+            text="Yes",
+            variable=self.auto_reboot_var,
+            value="Yes",
+            font=self.FONT_SMALL,
+        )
+        self.auto_reboot_yes.grid(row=4, column=0, pady=10)
+
+        self.auto_reboot_no = tk.Radiobutton(
+            self.eai_setup_window,
+            text="No",
+            variable=self.auto_reboot_var,
+            value="No",
+            font=self.FONT_SMALL,
+        )
+        self.auto_reboot_no.grid(row=5, column=0, pady=10)
+        # Create labels and entry boxes for passwords
+        self.passwords_label = tk.Label(
+            self.eai_setup_window,
+            text="Passwords:",
+            font=self.FONT_MEDIUM,
+        )
+        self.passwords_label.grid(row=6, column=0, pady=10)
+
+        self.hb_password_label = tk.Label(
+            self.eai_setup_window,
+            text="HB Password:",
+            font=self.FONT_SMALL,
+        )
+        self.hb_password_label.grid(row=7, column=0, pady=10)
+
+        self.hb_password_entry = tk.Entry(
+            self.eai_setup_window,
+            font=self.FONT_SMALL,
+        )
+        self.hb_password_entry.grid(row=8, column=0, pady=10)
+
+        self.ho_password_label = tk.Label(
+            self.eai_setup_window,
+            text="HO Password:",
+            font=self.FONT_SMALL,
+        )
+        self.ho_password_label.grid(row=9, column=0, pady=10)
+
+        self.ho_password_entry = tk.Entry(
+            self.eai_setup_window,
+            font=self.FONT_SMALL,
+        )
+        self.ho_password_entry.grid(row=10, column=0, pady=10)
+        # Create label and radio buttons for repository update type
+        self.repo_update_label = tk.Label(
+            self.eai_setup_window,
+            text="Repository Update Type:",
+            font=self.FONT_MEDIUM,
+        )
+        self.repo_update_label.grid(row=11, column=0, pady=10)
+
+        self.repo_update_var = tk.StringVar()
+        self.repo_update_var.set("Update")
+
+        self.repo_update_update = tk.Radiobutton(
+            self.eai_setup_window,
+            text="Update",
+            variable=self.repo_update_var,
+            value="Update",
+            font=self.FONT_SMALL,
+        )
+        self.repo_update_update.grid(row=12, column=0, pady=10)
+
+        self.repo_update_replace = tk.Radiobutton(
+            self.eai_setup_window,
+            text="Replace",
+            variable=self.repo_update_var,
+            value="Replace",
+            font=self.FONT_SMALL,
+        )
+        self.repo_update_replace.grid(row=13, column=0, pady=10)
+
+        # Create install button
+        self.install_button = tk.Button(
+            self.eai_setup_window,
+            text="Install",
+            command=self.install_eai,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.install_button.grid(row=14, column=0, pady=10)
+    def install_eai(self):
+        """Install and configure EAI with the selected options"""
+        # Get input values from setup window
+        installation_type = self.installation_type_var.get()
+        auto_reboot = self.auto_reboot_var.get()
+        hb_password = self.hb_password_entry.get()
+        ho_password = self.ho_password_entry.get()
+        repo_update_type = self.repo_update_var.get()
+
+        # Validate input values
+        if not hb_password:
+            messagebox.showerror("Error", "HB Password is required")
+            return
+
+        if not ho_password:
+            messagebox.showerror("Error", "HO Password is required")
+            return
+
+        # TODO: Implement installation logic
+        print("EAI installation complete")
+
+    def display_password_update(self):
+        """Display window for updating EAI passwords"""
+        # Create new top level window for password update
+        self.password_update_window = tk.Toplevel(self.eai_options_window)
+        self.password_update_window.title("EAI Password Update")
+        self.password_update_window.geometry("400x400")
+
+        # Create labels and entry boxes for passwords
+        self.passwords_label = tk.Label(
+            self.password_update_window,
+            text="Passwords:",
+            font=self.FONT_MEDIUM,
+        )
+        self.hb_password_label = tk.Label(
+            self.password_update_window,
+            text="HB Password:",
+            font=self.FONT_SMALL,
+        )
+        self.hb_password_label.grid(row=1, column=0, pady=10)
+
+        self.hb_password_entry = tk.Entry(
+            self.password_update_window,
+            font=self.FONT_SMALL,
+        )
+        self.hb_password_entry.grid(row=2, column=0, pady=10)
+
+        self.ho_password_label = tk.Label(
+            self.password_update_window,
+            text="HO Password:",
+            font=self.FONT_SMALL,
+        )
+        self.ho_password_label.grid(row=3, column=0, pady=10)
+
+        self.ho_password_entry = tk.Entry(
+            self.password_update_window,
+            font=self.FONT_SMALL,
+        )
+        self.ho_password_entry.grid(row=4, column=0, pady=10)
+
+        # Create update button
+        self.update_button = tk.Button(
+            self.password_update_window,
+            text="Update",
+            command=self.update_passwords,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.update_button.grid(row=5, column=0, pady=10)
+    def update_passwords(self):
+        """Encrypt and store updated EAI passwords"""
+        # Get input values from password update window
+        hb_password = self.hb_password_entry.get()
+        ho_password = self.ho_password_entry.get()
+
+        # Validate input values
+        if not hb_password:
+            messagebox.showerror("Error", "HB Password is required")
+            return
+
+        if not ho_password:
+            messagebox.showerror("Error", "HO Password is required")
+            return
+
+        # TODO: Encrypt and store passwords
+        print("Passwords updated")
+
+    def display_etl_options(self):
+        """Display options for setting up ETL"""
+        # Create new top level window for ETL options
+        self.etl_options_window = tk.Toplevel(self.top_level_window)
+        self.etl_options_window.title("ETL Options")
+        self.etl_options_window.geometry("400x400")
+        # Create button for DataStage setup
+        self.ds_button = tk.Button(
+            self.etl_options_window,
+            text="DataStage",
+            command=self.display_etl_setup_options,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.ds_button.grid(row=0, column=0, pady=10)
+
+        # Create button for ETL password update
+        self.etl_password_update_button = tk.Button(
+            self.etl_options_window,
+            text="Password Update",
+            command=self.display_etl_password_update,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.etl_password_update_button.grid(row=1, column=0, pady=10)
+    def display_etl_setup_options(self):
+        """Display options for setting up DataStage"""
+        # Create new top level window for DataStage setup options
+        self.etl_setup_window = tk.Toplevel(self.etl_options_window)
+        self.etl_setup_window.title("DataStage Setup Options")
+        self.etl_setup_window.geometry("400x500")
+
+        # Create label and radio buttons for installation type
+        self.installation_type_label = tk.Label(
+            self.etl_setup_window,
+            text="Unattended Installation:",
+            font=self.FONT_MEDIUM,
+        )
+        self.installation_type_label.grid(row=0, column=0, pady=10)
+
+        self.installation_type_var = tk.StringVar()
+        self.installation_type_var.set("Yes")
+
+        self.installation_type_yes = tk.Radiobutton(
+            self.etl_setup_window,
+            text="Yes",
+            variable=self.installation_type_var,
+            value="Yes",
+            font=self.FONT_SMALL,
+        )
+        self.installation_type_yes.grid(row=1, column=0, pady=10)
+
+        self.installation_type_no = tk.Radiobutton(
+            self.etl_setup_window,
+            text="No",
+            variable=self.installation_type_var,
+            value="No",
+            font=self.FONT_SMALL,
+        )
+        self.installation_type_no.grid(row=2, column=0, pady=10)
+
+        # Create label and entry box for HB password
+        self.hb_password_label = tk.Label(
+            self.etl_setup_window,
+            text="HB Password:",
+            font=self.FONT_MEDIUM,
+        )
+        self.hb_password_label.grid(row=3, column=0, pady=10)
+
+        self.hb_password_entry = tk.Entry(
+            self.etl_setup_window,
+            font=self.FONT_MEDIUM,
+        )
+        self.hb_password_entry.grid(row=4, column=0, pady=10)
+        # Create label and radio buttons for repository update type
+        self.repo_update_label = tk.Label(
+            self.etl_setup_window,
+            text="Repository Update Type:",
+            font=self.FONT_MEDIUM,
+        )
+        self.repo_update_label.grid(row=5, column=0, pady=10)
+
+        self.repo_update_var = tk.StringVar()
+        self.repo_update_var.set("Update")
+
+        self.repo_update_update = tk.Radiobutton(
+            self.etl_setup_window,
+            text="Update",
+            variable=self.repo_update_var,
+            value="Update",
+            font=self.FONT_SMALL,
+        )
+        self.repo_update_update.grid(row=6, column=0, pady=10)
+
+        self.repo_update_replace = tk.Radiobutton(
+            self.etl_setup_window,
+            text="Replace",
+            variable=self.repo_update_var,
+            value="Replace",
+            font=self.FONT_SMALL,
+        )
+        self.repo_update_replace.grid(row=7, column=0, pady=10)
+        # Create install button
+        self.install_button = tk.Button(
+            self.etl_setup_window,
+            text="Install",
+            command=self.install_etl,
+            width=20,
+            height=2,
+            bg=self.COLOR_PRIMARY,
+            fg=self.COLOR_SECONDARY,
+            font=self.FONT_MEDIUM,
+        )
+        self.install_button.grid(row=8, column=0, pady=10)
+
+    def install_etl(self):
+        """Install and configure DataStage with the selected options"""
+        # Get input values from setup window
+        installation_type = self.installation_type_var.get()
+        hb_password = self.hb_password_entry.get()
+        repo_update_type = self.repo_update_var.get()
+
+        # Validate input values
+        if not hb_password:
+            messagebox.showerror("Error", "HB Password is required")
+            return
+
+        # TODO: Implement installation logic
+        print("DataStage installation complete")```
+    def create_help_window(self):
+        """Create window with information on how to use the GUI"""
+        # Create new top level window for help information
+        self.help_window = tk.Toplevel(self.top_level_window)
+        self.help_window.title("Help")
+        self.help_window.geometry("400x400")
+
+        # Create label with help information
+        help_text = """
+        This GUI is used to install and configure various applications. To get started, click on the buttons for the application you want to install.
+        The options for that application will be displayed in a new window. Enter the required information and click on the 'Install' button to begin the installation process.
+        If you need help, please contact the administrator.
+        """
+        self.help_label = tk.Label(
+            self.help_window,
+            text=help_text,
+            font=self.FONT_MEDIUM,
+            justify="left",
+        )
+        self.help_label.pack(pady=10, padx=10)
+    def run(self):
+        """Run the GUI application"""
+        self.top_level_window.mainloop()
+
+    def main():
+        """Create and run the GUI application"""
+        app = ApplicationInstaller()
+        app.run()
+
+    if __name__ == "__main__":
+        main()
