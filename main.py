aa8ee423875a4d9dafe126913c7c4b7a8cd8e165
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-17 10:35:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,62 @@ class InstallOrchestration:
 
     def setup_system_options_styles(self):
         self.system_options_window.option_add("*Button.Background", "#4d4d4d")
+        self.system_options_window.option_add("*Button.Foreground", "white")
+        self.system_options_window.option_add("*Button.activeBackground", "#808080")
+        self.system_options_window.option_add("*Button.activeForeground", "white")
+        self.system_options_window.option_add("*Button.highlightThickness", 0)
+        self.system_options_window.option_add("*Label.Background", "#d9d9d9")
+        self.system_options_window.option_add("*Label.Foreground", "#4d4d4d")
 
+    def displaySystemSetupOptions(self, system_type, install_type):
+        # Create new top level window
+        self.system_setup_options_window = tk.Toplevel(self.master)
+        self.system_setup_options_window.title(f"{system_type} {install_type} Setup")
+
+        # Set up custom styles for this window
+        self.setup_system_setup_options_styles()
+
+        # Create labels and options
+        self.installation_type_var = self.create_label_and_options("Unattended installation", ["Yes", "No"])
+        self.repo_update_var = self.create_label_and_options("Repository", ["Update", "Replace"])
+        self.hb_password_entry = self.create_label_and_entry("HB Password")
+        if install_type in ["IIB10", "ACE12"]:
+            self.ho_password_entry = self.create_label_and_entry("HO Password")
+            self.auto_reboot_var = self.create_label_and_options("Reboot Automatically", ["Yes", "No"])
+
+        # Create submit button
+        submit_button = ttk.Button(self.system_setup_options_window, text="Submit", command=lambda: self.performInstallation(system_type, install_type), style="SubmitButton.TButton")
+        submit_button.pack(pady=10)
+
+        # Create quit button
+        quit_button = ttk.Button(self.system_setup_options_window, text="Quit", command=self.system_setup_options_window.destroy, style="QuitButton.TButton")
+        quit_button.pack(pady=10)
+        self.system_options_window.destroy()
+
+    def setup_system_setup_options_styles(self):
+        self.system_setup_options_window.option_add("*Button.Background", "#4d4d4d")
+        self.system_setup_options_window.option_add("*Button.Foreground", "white")
+        self.system_setup_options_window.option_add("*Button.activeBackground", "#808080")
+        self.system_setup_options_window.option_add("*Button.activeForeground", "white")
+        self.system_setup_options_window.option_add("*Button.highlightThickness", 0)
+        self.system_setup_options_window.option_add("*Label.Background", "#d9d9d9")
+        self.system_setup_options_window.option_add("*Label.Foreground", "#4d4d4d")
+
+    def create_label_and_options(self, label_text, option_values):
+        label = ttk.Label(self.system_setup_options_window, text=label_text, style="OptionLabel.TLabel")
+        label.pack(pady=5)
+        var = tk.StringVar(value=option_values[0])
+        for option_value in option_values:
+            radio = ttk.Radiobutton(self.system_setup_options_window, text=option_value, variable=var, value=option_value, style="Option.TRadiobutton")
+            radio.pack()
+        return var
+
+    def create_label_and_entry(self, label_text):
+        label = ttk.Label(self.system_setup_options_window, text=label_text, style="OptionLabel.TLabel")
+        label.pack(pady=5)
+        entry = ttk.Entry(self.system_setup_options_window, show="*", style="Option.TEntry")
+        entry.pack()
+        return entry
     def passwordUpdate(self):
         # Create new top level window
         self.password_update_window = tk.Toplevel(self.master)
