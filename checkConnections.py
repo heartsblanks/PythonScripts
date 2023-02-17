27cbeb15e6b050e5f072cb0dcd9f458b89f1d1c0
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-17 01:26:22 +0100

    Update checkConnections.py

--- checkConnections.py
+++ checkConnections.py
@@ -1,26 +1,36 @@
-import subprocess
+import os
+import logging
+import socket
 
-class checkConnections:
+class CheckConnections:
     def __init__(self):
         pass
-
+    
     def check_git_connection(self):
         try:
-            subprocess.check_call(["git", "--version"])
-            return True
-        except subprocess.CalledProcessError:
-            return False
-
+            os.system("git ls-remote")
+            logging.info("Git connection successful")
+        except:
+            message = "Could not connect to Git. Please check your internet connection and Git configuration."
+            logging.error(message)
+            raise Exception(message)
+    
     def check_cvs_connection(self):
+        cvs_host = "cvs.example.com"
+        cvs_port = 2401
         try:
-            subprocess.check_call(["cvs", "--version"])
-            return True
-        except subprocess.CalledProcessError:
-            return False
-
+            socket.create_connection((cvs_host, cvs_port), timeout=5)
+            logging.info("CVS connection successful")
+        except:
+            message = "Could not connect to CVS. Please check your internet connection and CVS configuration."
+            logging.error(message)
+            raise Exception(message)
+    
     def check_command(self, command):
         try:
-            subprocess.check_call(command)
-            return True
-        except subprocess.CalledProcessError:
-            return False
+            os.system(command)
+            logging.info(f"Command '{command}' successful")
+        except:
+            message = f"Command '{command}' failed. Please check that it is installed and on the PATH."
+            logging.error(message)
+            raise Exception(message)
