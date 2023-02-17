ecdf3791de403a732f3da2ba8471db2b40683519
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-17 01:24:54 +0100

    Update createVariables.py

--- createVariables.py
+++ createVariables.py
@@ -2,19 +2,25 @@ import os
 import csv
 from tkinter import messagebox
 from tkinter import filedialog
+import logging
 
 class createVariables:
     def __init__(self, system_type):
         self.system_type = system_type
+        self.logger = logging.getLogger(__name__)
 
     def create(self):
-        with open("variable_details.csv", "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if row[0] == "Y" and self.system_type == "EAI":
-                    self.create_variable(row)
-                elif row[1] == "Y" and self.system_type == "ETL":
-                    self.create_variable(row)
+        try:
+            with open("variable_details.csv", "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row[0] == "Y" and self.system_type == "EAI":
+                        self.create_variable(row)
+                    elif row[1] == "Y" and self.system_type == "ETL":
+                        self.create_variable(row)
+        except Exception as e:
+            self.logger.error(f"Error while reading variable_details.csv: {str(e)}")
+            raise e
 
     def create_variable(self, row):
         var_name = row[2]
@@ -22,36 +28,41 @@ class createVariables:
         update_path = row[4]
         var_type = row[5]
 
-        if update_path == "Y":
-            if os.path.isabs(var_value):
-                if os.path.exists(var_value):
-                    os.environ["PATH"] += ";" + var_value
+        try:
+            if update_path == "Y":
+                if os.path.isabs(var_value):
+                    if os.path.exists(var_value):
+                        os.environ["PATH"] += ";" + var_value
+                    else:
+                        self.show_error_message(row)
+                        self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
                 else:
-                    self.show_error_message(row)
-            else:
-                os.environ["PATH"] += ";" + var_value
+                    os.environ["PATH"] += ";" + var_value
 
-        if var_type == "E":
-            if os.path.isabs(var_value):
-                if os.path.exists(var_value):
+            if var_type == "E":
+                if os.path.isabs(var_value):
+                    if os.path.exists(var_value):
+                        os.environ[var_name] = var_value
+                        self.__dict__[var_name] = var_value
+                    else:
+                        self.show_error_message(row)
+                        self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
+                else:
                     os.environ[var_name] = var_value
                     self.__dict__[var_name] = var_value
-                else:
-                    self.show_error_message(row)
-            else:
-                os.environ[var_name] = var_value
-                self.__dict__[var_name] = var_value
 
-        elif var_type == "I":
-            if os.path.isabs(var_value):
-                if os.path.exists(var_value):
+            elif var_type == "I":
+                if os.path.isabs(var_value):
+                    if os.path.exists(var_value):
+                        self.__dict__[var_name] = var_value
+                    else:
+                        self.show_error_message(row)
+                        self.logger.error(f"Variable {var_name} could not be created. {var_value} is not a valid path.")
+                else:
                     self.__dict__[var_name] = var_value
-                else:
-                    self.show_error_message(row)
-            else:
-                self.__dict__[var_name] = var_value
-
-
+        except Exception as e:
+            self.logger.error(f"Error while creating variable {var_name}: {str(e)}")
+            raise e
 
     def show_error_message(self, row):
         message = f"Could not create variable for {row[2]} with value {row[3]}. Please update the variable_details.csv file with a valid path."
