38077c314080eb29786ee3ab35316b71aad256e5
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-17 01:32:00 +0100

    Update PluginInstaller.py

--- PluginInstaller.py
+++ PluginInstaller.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import urllib.request
 import zipfile
+import logging
 
 # Local file paths for plugin installation
 IIB_TOOLKIT_PLUGIN_PATH = r"C:\temp\iib_toolkit_plugin.zip"
@@ -10,6 +11,7 @@ MAVEN_CLI_PATH = r"C:\apache-maven-3.8.1-bin.zip"
 ECLIPSE_PLUGIN_URL = "https://download.eclipse.org/releases/latest"
 MAVEN_PLUGIN_URL = "https://repo1.maven.org/maven2/com/github/eirslett/maven-frontend-plugin/1.10.0/maven-frontend-plugin-1.10.0.jar"
 
+logger = logging.getLogger(__name__)
 
 class PluginInstaller:
     def __init__(self, system_type):
@@ -17,21 +19,21 @@ class PluginInstaller:
 
     def install_iib_toolkit_plugin(self):
         if self.system_type == "EAI":
-            self.install_plugin_from_zip(IIB_TOOLKIT_PLUGIN_PATH)
+            self.install_plugin_from_zip(IIB_TOOLKIT_PLUGIN_PATH, "IIB Toolkit")
 
     def install_ace_toolkit_plugin(self):
         if self.system_type == "EAI":
-            self.install_plugin_from_zip(ACE_TOOLKIT_PLUGIN_PATH)
+            self.install_plugin_from_zip(ACE_TOOLKIT_PLUGIN_PATH, "ACE Toolkit")
 
     def install_eclipse_plugin(self):
         eclipse_path = os.environ.get("ECLIPSE_HOME")
         if not eclipse_path:
-            print("ECLIPSE_HOME environment variable not set.")
+            logger.error("ECLIPSE_HOME environment variable not set.")
             return
         try:
             urllib.request.urlretrieve(ECLIPSE_PLUGIN_URL, os.path.join(eclipse_path, "eclipse", "plugins", "latest"))
         except:
-            print("Failed to install Eclipse plugin.")
+            logger.exception("Failed to install Eclipse plugin.")
 
     def install_maven_cli(self):
         try:
@@ -41,32 +43,32 @@ class PluginInstaller:
             os.environ["MAVEN_HOME"] = "C:\\apache-maven-3.8.1"
             os.environ["PATH"] = f"{os.environ['MAVEN_HOME']}\\bin;{os.environ['PATH']}"
         except:
-            print("Failed to install Maven CLI.")
+            logger.exception("Failed to install Maven CLI.")
 
     def install_jre(self):
         java_home = os.environ.get("JAVA_HOME")
         if not java_home:
-            print("JAVA_HOME environment variable not set.")
+            logger.error("JAVA_HOME environment variable not set.")
             return
         try:
             subprocess.run([os.path.join(java_home, "bin", "java"), "-version"])
         except:
-            print("Failed to install JRE.")
+            logger.exception("Failed to install JRE.")
 
     def install_maven_plugin(self):
         try:
             urllib.request.urlretrieve(MAVEN_PLUGIN_URL, "maven-frontend-plugin.jar")
             shutil.move("maven-frontend-plugin.jar", os.path.join(os.environ["MAVEN_HOME"], "lib", "ext"))
         except:
-            print("Failed to install Maven plugin.")
+            logger.exception("Failed to install Maven plugin.")
 
-    def install_plugin_from_zip(self, plugin_path):
+    def install_plugin_from_zip(self, plugin_path, plugin_name):
         plugin_dir = os.environ.get("ECLIPSE_HOME")
         if not plugin_dir:
-            print("ECLIPSE_HOME environment variable not set.")
+            logger.error("ECLIPSE_HOME environment variable not set.")
             return
         try:
             with zipfile.ZipFile(plugin_path, "r") as zip_ref:
                 zip_ref.extractall(plugin_dir)
         except:
-            print(f"Failed to install {plugin_path}.")
+            logger.exception(f"Failed to install {plugin_name} plugin from zip.")
