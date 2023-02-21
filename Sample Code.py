d9de04461e805331ce15b3df88c46147d41ee336
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-21 17:20:36 +0100

    Update Sample Code.py

--- Sample Code.py
+++ Sample Code.py
@@ -1,59 +1,28 @@
-import os
+import csv
 import json
-import logging
 
+csv_file = 'data.csv'
 json_file = 'data.json'
 
-with open(json_file, 'r') as file:
-    data = json.load(file)
+data = {'IIB10': {'Files': [], 'Directories': [], 'Variables': []}, 'EAI': {'Files': [], 'Directories': [], 'Variables': []},
+        'ETL': {'Variables': []}, 'ACE12': {'Files': [], 'Directories': [], 'Variables': []},
+        'DS': {'Variables': []}, 'GIT': {'Variables': []},
+        'CVS': {'Variables': []}, 'MAVEN': {'Variables': []}, 'Common': {'Variables': []}}
 
-class Constants:
-    def __init__(self, env_type):
-        if env_type in ['IIB10', 'ACE12', 'EAI', 'ETL', 'DS', 'GIT', 'CVS', 'MAVEN', 'Common']:
-            self.env_type = env_type
-            for item in data[env_type]:
-                if item['Type'] == 'E':
-                    try:
-                        if os.path.isabs(item['Value']):
-                            if os.path.isdir(item['Value']) or os.path.isfile(item['Value']):
-                                os.environ[item['Name']] = item['Value']
-                                setattr(self, item['Name'], item['Value'])
-                                if item['Path'] == 'Y':
-                                    os.environ['PATH'] += os.pathsep + item['Value']
-                            else:
-                                raise ValueError(f"{item['Value']} is not a valid directory or file path.")
-                        else:
-                            os.environ[item['Name']] = item['Value']
-                            setattr(self, item['Name'], item['Value'])
-                    except Exception as e:
-                        logging.error(str(e))
-                elif item['Type'] == 'I':
-                    try:
-                        if os.path.isabs(item['Value']):
-                            if os.path.isdir(item['Value']) or os.path.isfile(item['Value']):
-                                setattr(self, item['Name'], item['Value'])
-                                if item['Path'] == 'Y':
-                                    os.environ['PATH'] += os.pathsep + item['Value']
-                            else:
-                                raise ValueError(f"{item['Value']} is not a valid directory or file path.")
-                        else:
-                            setattr(self, item['Name'], item['Value'])
-                    except Exception as e:
-                        logging.error(str(e))
-        else:
-            raise ValueError(f"{env_type} is not a valid environment type.")
+column_types = {'IIB10': ['Files', 'Directories', 'Variables'], 'EAI': ['Files', 'Directories', 'Variables'],
+                'ETL': ['Variables'], 'ACE12': ['Files', 'Directories', 'Variables'],
+                'DS': ['Variables'], 'GIT': ['Variables'], 'CVS': ['Variables'], 'MAVEN': ['Variables'], 'Common': ['Variables']}
 
+with open(csv_file, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        for env_type, col_types in column_types.items():
+            if row[env_type] == 'Y':
+                for col_type in col_types:
+                    if row[col_type] == 'Y':
+                        data[env_type][col_type].append({'Name': row['Name'], 'Value': row['Value'], 'Type': row['Type'], 'Path': row['Path']})
+                    elif not col_types.count('Directories') and not col_types.count('Files'):
+                        data[env_type]['Variables'].append({'Name': row['Name'], 'Value': row['Value'], 'Type': row['Type'], 'Path': row['Path']})
 
-from Constants import Constants
-import logging
-
-logging.basicConfig(filename='constants.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s')
-
-try:
-    constants = Constants('IIB10')
-    print(constants.BROKER_URL)
-    print(constants.MQ_HOST)
-    print(constants.MQ_PORT)
-except Exception as e:
-    logging.error(str(e))
-    print(str(e))
+with open(json_file, 'w') as file:
+    json.dump(data, file, indent=4)
