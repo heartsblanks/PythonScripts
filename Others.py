7f1211d743ef7288a678ec70776fb68ff3d8ea7f
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-03 12:28:03 +0100

    Update Others.py

--- Others.py
+++ Others.py
@@ -15,3 +15,21 @@ encrypted = output.split('=')[1].strip()
 # Set the encrypted password as an environment variable
 import os
 os.environ['ENCRYPTED'] = encrypted
+
+import subprocess
+
+# Define the PLINK command to execute
+plink_path = 'C:/path/to/plink.exe'
+putty_private_key = 'C:/path/to/putty-private-key.ppk'
+plink_command = f'{plink_path} -no-antispoof -i {putty_private_key} user@host command-to-execute'
+
+# Execute the command and capture its output
+output = subprocess.run(plink_command, capture_output=True, text=True).stdout.strip()
+
+# Print the output
+print(output)
+
+command = f'{PLINK_PATH} -no-antispoof -i {PUTTY_PRIVATE_KEY} user@host "echo {USER_PASSWORD} | sudodeploy -encrypt 2>/dev/null | tail -n1"'
+
+
+
