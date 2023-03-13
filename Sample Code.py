1ffae8b9d35720fe9733b98e89f97f6f3806f9ee
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-13 15:11:24 +0100

    Update Sample Code.py

--- Sample Code.py
+++ Sample Code.py
@@ -1,18 +1,9 @@
 import os
 import sys
-import xml.etree.ElementTree as ET
-from com.ibm.etools.msg.client import MessageSetClient
-from com.ibm.etools.msg.util import MessageUtil
-from com.ibm.etools.msg.model import MessageType
-from com.ibm.etools.msg.model import Element
-from com.ibm.etools.msg.model import Field
-from com.ibm.etools.msg.model import Group
-from com.ibm.etools.msg.model import ListElement
-from com.ibm.etools.msg.model import ComplexType
-from com.ibm.etools.msg.model import EnumerationType
-from com.ibm.etools.msg.model import MessageModel
-from com.ibm.etools.msg.util import TypeReference
-from com.ibm.etools.msg.model import MessageSet
+import ibmIIBApi
+
+# Load the IIB Toolkit API
+iib = ibmIIBApi.IibToolkit()
 
 # Set the path to the message set project directory
 msgset_dir = "/path/to/message/set/project"
@@ -26,99 +17,15 @@ msgset_name = "MyMessageSet"
 # Set the name of the output DFDL message model
 dfdl_name = "MyDfdlModel"
 
-# Load the message set project
-client = MessageSetClient(msgset_dir, False, True)
-messageSet = client.getMessageSet(msgset_name)
-
 # Create a new DFDL message model
-messageModel = MessageModel()
-messageModel.setName(dfdl_name)
+dfdl = iib.create_dfdl_model(dfdl_name)
 
-# Iterate through the message set elements and add them to the DFDL model
-for message in messageSet.getMessages():
-    messageType = MessageType()
-    messageType.setName(message.getName())
-    messageModel.addType(messageType)
-    for element in message.getElements():
-        addElementToModel(messageType, element)
+# Import the message set data into the DFDL model
+msgset = iib.load_message_set(msgset_name, msgset_dir)
+dfdl.import_message_set(msgset)
 
 # Save the DFDL schema to a file
 dfdl_file = os.path.join(dfdl_dir, dfdl_name + ".xsd")
-with open(dfdl_file, "w") as f:
-    f.write(MessageUtil.generateXSD(messageModel))
+dfdl.save(dfdl_file)
 
 print("DFDL schema created: {}".format(dfdl_file))
-
-# Function to add a message set element to the DFDL model
-def addElementToModel(parentType, element):
-    if isinstance(element, Group):
-        groupType = ComplexType()
-        groupType.setName(element.getName())
-        groupType.setMinOccurs(element.getMinOccurs())
-        groupType.setMaxOccurs(element.getMaxOccurs())
-        parentType.addElement(groupType)
-        for childElement in element.getElements():
-            addElementToModel(groupType, childElement)
-    elif isinstance(element, ListElement):
-        listType = ComplexType()
-        listType.setName(element.getName())
-        listType.setMinOccurs(element.getMinOccurs())
-        listType.setMaxOccurs(element.getMaxOccurs())
-        parentType.addElement(listType)
-        listRef = TypeReference()
-        listRef.setType(listType)
-        listRef.setName("item")
-        listType.addTypeReference(listRef)
-        for childElement in element.getElements():
-            addElementToModel(listType, childElement)
-    else:
-        fieldType = Field()
-        fieldType.setName(element.getName())
-        fieldType.setMinOccurs(element.getMinOccurs())
-        fieldType.setMaxOccurs(element.getMaxOccurs())
-        fieldType.setType(getFieldType(element))
-        parentType.addElement(fieldType)
-
-# Function to determine the DFDL data type for a message set element
-def getFieldType(element):
-    if isinstance(element, Element):
-        if element.getType() == "char":
-            return "xs:string"
-        elif element.getType() == "int":
-            return "xs:int"
-        elif element.getType() == "decimal":
-            return "xs:decimal"
-        elif element.getType() == "float":
-            return "xs:float"
-        elif element.getType() == "double":
-            return "xs:double
-                elif element.getType() == "boolean":
-            return "xs:boolean"
-        elif element.getType() == "byte":
-            return "xs:byte"
-        elif element.getType() == "short":
-            return "xs:short"
-        elif element.getType() == "long":
-            return "xs:long"
-        elif element.getType() == "dateTime":
-            return "xs:dateTime"
-        elif element.getType() == "time":
-            return "xs:time"
-        elif element.getType() == "date":
-            return "xs:date"
-        elif element.getType() == "duration":
-            return "xs:duration"
-        elif element.getType() == "hexBinary":
-            return "xs:hexBinary"
-        elif element.getType() == "base64Binary":
-            return "xs:base64Binary"
-        elif element.getType() == "anyURI":
-            return "xs:anyURI"
-        elif element.getType() == "string":
-            if element.getMaxLength() > 0:
-                return "xs:string({})".format(element.getMaxLength())
-            else:
-                return "xs:string"
-    elif isinstance(element, EnumerationType):
-        return "xs:string"
-
