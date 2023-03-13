b1a5cdbcefe415ae2066900967440391e40dd3b4
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-13 14:32:59 +0100

    Update Sample Code.py

--- Sample Code.py
+++ Sample Code.py
@@ -1,28 +1,94 @@
-import csv
-import json
+import os
+import sys
+import xml.etree.ElementTree as ET
+from com.ibm.etools.msg.client import MessageSetClient
+from com.ibm.etools.msg.util import MessageUtil
+from com.ibm.etools.msg.model import MessageType
+from com.ibm.etools.msg.model import Element
+from com.ibm.etools.msg.model import Field
+from com.ibm.etools.msg.model import Group
+from com.ibm.etools.msg.model import ListElement
+from com.ibm.etools.msg.model import ComplexType
+from com.ibm.etools.msg.model import EnumerationType
+from com.ibm.etools.msg.model import MessageModel
+from com.ibm.etools.msg.util import TypeReference
+from com.ibm.etools.msg.model import MessageSet
 
-csv_file = 'data.csv'
-json_file = 'data.json'
+# Set the path to the message set project directory
+msgset_dir = "/path/to/message/set/project"
 
-data = {'IIB10': {'Files': [], 'Directories': [], 'Variables': []}, 'EAI': {'Files': [], 'Directories': [], 'Variables': []},
-        'ETL': {'Variables': []}, 'ACE12': {'Files': [], 'Directories': [], 'Variables': []},
-        'DS': {'Variables': []}, 'GIT': {'Variables': []},
-        'CVS': {'Variables': []}, 'MAVEN': {'Variables': []}, 'Common': {'Variables': []}}
+# Set the path to the DFDL schema output directory
+dfdl_dir = "/path/to/dfdl/schema/output"
 
-column_types = {'IIB10': ['Files', 'Directories', 'Variables'], 'EAI': ['Files', 'Directories', 'Variables'],
-                'ETL': ['Variables'], 'ACE12': ['Files', 'Directories', 'Variables'],
-                'DS': ['Variables'], 'GIT': ['Variables'], 'CVS': ['Variables'], 'MAVEN': ['Variables'], 'Common': ['Variables']}
+# Set the name of the message set project
+msgset_name = "MyMessageSet"
 
-with open(csv_file, 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        for env_type, col_types in column_types.items():
-            if row[env_type] == 'Y':
-                for col_type in col_types:
-                    if row[col_type] == 'Y':
-                        data[env_type][col_type].append({'Name': row['Name'], 'Value': row['Value'], 'Type': row['Type'], 'Path': row['Path']})
-                    elif not col_types.count('Directories') and not col_types.count('Files'):
-                        data[env_type]['Variables'].append({'Name': row['Name'], 'Value': row['Value'], 'Type': row['Type'], 'Path': row['Path']})
+# Set the name of the output DFDL message model
+dfdl_name = "MyDfdlModel"
 
-with open(json_file, 'w') as file:
-    json.dump(data, file, indent=4)
+# Load the message set project
+client = MessageSetClient(msgset_dir, False, True)
+messageSet = client.getMessageSet(msgset_name)
+
+# Create a new DFDL message model
+messageModel = MessageModel()
+messageModel.setName(dfdl_name)
+
+# Iterate through the message set elements and add them to the DFDL model
+for message in messageSet.getMessages():
+    messageType = MessageType()
+    messageType.setName(message.getName())
+    messageModel.addType(messageType)
+    for element in message.getElements():
+        addElementToModel(messageType, element)
+
+# Save the DFDL schema to a file
+dfdl_file = os.path.join(dfdl_dir, dfdl_name + ".xsd")
+with open(dfdl_file, "w") as f:
+    f.write(MessageUtil.generateXSD(messageModel))
+
+print("DFDL schema created: {}".format(dfdl_file))
+
+# Function to add a message set element to the DFDL model
+def addElementToModel(parentType, element):
+    if isinstance(element, Group):
+        groupType = ComplexType()
+        groupType.setName(element.getName())
+        groupType.setMinOccurs(element.getMinOccurs())
+        groupType.setMaxOccurs(element.getMaxOccurs())
+        parentType.addElement(groupType)
+        for childElement in element.getElements():
+            addElementToModel(groupType, childElement)
+    elif isinstance(element, ListElement):
+        listType = ComplexType()
+        listType.setName(element.getName())
+        listType.setMinOccurs(element.getMinOccurs())
+        listType.setMaxOccurs(element.getMaxOccurs())
+        parentType.addElement(listType)
+        listRef = TypeReference()
+        listRef.setType(listType)
+        listRef.setName("item")
+        listType.addTypeReference(listRef)
+        for childElement in element.getElements():
+            addElementToModel(listType, childElement)
+    else:
+        fieldType = Field()
+        fieldType.setName(element.getName())
+        fieldType.setMinOccurs(element.getMinOccurs())
+        fieldType.setMaxOccurs(element.getMaxOccurs())
+        fieldType.setType(getFieldType(element))
+        parentType.addElement(fieldType)
+
+# Function to determine the DFDL data type for a message set element
+def getFieldType(element):
+    if isinstance(element, Element):
+        if element.getType() == "char":
+            return "xs:string"
+        elif element.getType() == "int":
+            return "xs:int"
+        elif element.getType() == "decimal":
+            return "xs:decimal"
+        elif element.getType() == "float":
+            return "xs:float"
+        elif element.getType() == "double":
+            return "xs:double
