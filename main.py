934eb4ba5c8fd343f0b9999d2b6140b3dea6e3a5
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-14 11:47:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,16 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 
-# Parse the IIB messageSet.mset file
-tree = ET.parse('messageSet.mset')
-root = tree.getroot()
+# Load the MXSD file
+mxsd_doc = etree.parse('example.mxsd')
 
-# Extract the message set name
-message_set_name = root.find('.//messageSet').get('name')
+# Set the namespace mapping for MXSD
+mxsd_ns = {'mx': 'http://schemas.microsoft.com/2003/10/Serialization/'}
 
-# Extract the message formats
-message_formats = {}
-for format_elem in root.findall('.//messageFormat'):
-    format_name = format_elem.get('name')
-    message_formats[format_name] = []
-    for field_elem in format_elem.findall('.//field'):
-        field_name = field_elem.get('name')
-        field_type = field_elem.get('type')
-        message_formats[format_name].append((field_name, field_type))
+# Convert the MXSD to XSD
+for element in mxsd_doc.xpath('//mx:*', namespaces=mxsd_ns):
+    element.tag = etree.QName(element.tag).localname
 
-# Map the IIB message formats and field definitions to their DFDL equivalents
-dfdl_formats = {}
-for format_name, fields in message_formats.items():
-    dfdl_fields = []
-    for field_name, field_type in fields:
-        # Map the field types to DFDL types
-        if field_type == 'string':
-            dfdl_type = 'xs:string'
-        elif field_type == 'integer':
-            dfdl_type = 'xs:int'
-        elif field_type == 'decimal':
-            dfdl_type = 'xs:decimal'
-        # Add the field to the DFDL format
-        dfdl_fields.append(f'<dfdl:element name="{field_name}" type="{dfdl_type}"/>')
-    # Combine the fields into a DFDL schema
-    dfdl_schema = f'<dfdl:format xmlns:dfdl="http://www.ogf.org/dfdl/dfdl-1.0/" xmlns:xs="http://www.w3.org/2001/XMLSchema">{"".join(dfdl_fields)}</dfdl:format>'
-    dfdl_formats[format_name] = dfdl_schema
-
-# Generate the DFDL file using a template or a DFDL schema generator tool
-dfdl_template = f'''<?xml version="1.0" encoding="UTF-8"?>
-<dfdl:define xmlns:dfdl="http://www.ogf.org/dfdl/dfdl-1.0/">
-    <dfdl:format ref="{message_set_name}"/>
-    {"".join(dfdl_formats.values())}
-</dfdl:define>
-'''
-
-# Write the DFDL file to disk or output it to the console
-with open('messageSet.dfdl', 'w') as f:
-    f.write(dfdl_template)
-
-print("DFDL file created successfully.")
+# Remove the MXSD namespace declaration
+xsd_doc = etree.ElementTree(mxsd_doc.getroot())
+xsd_doc.getroot().nsmap.clear()
+xsd_doc.write('example.xsd', pretty_print=True)
