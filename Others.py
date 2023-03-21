b4f90faf96708e197ff97c58b87bb35d0e4603d2
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-21 09:54:39 +0100

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,35 +1,17 @@
 from lxml import etree
 
-# Specify the tag name to extract
+# Set the tag name to search for
 tag_name = 'page'
 
-# Read the XML file into memory and parse it using lxml
-try:
-    with open('original_file.xml', 'r') as file:
-        xml_string = file.read()
-    root = etree.fromstring(xml_string)
-except OSError as e:
-    print(f'Error reading/parsing XML file: {e}')
-    exit()
-except etree.XMLSyntaxError as e:
-    print(f'Error parsing XML file: {e}')
-    exit()
+# Open the XML file and create an iterator for efficient processing
+with open('large_file.xml', 'rb') as file:
+    context = etree.iterparse(file, events=('start',), tag=tag_name)
 
-# Extract all elements with the specified tag name from the XML tree
-elements = root.xpath(f'//{tag_name}')
+    # Loop through each matching element and print its line
+    for event, elem in context:
+        print(etree.tostring(elem, encoding='unicode'))
 
-# Create a new XML tree with only the extracted elements
-new_root = etree.Element('root')
-for element in elements:
-    new_root.append(element)
-
-# Convert the new XML tree to a string with customized formatting options
-pretty_xml = etree.tostring(new_root, encoding='unicode', pretty_print=True, xml_declaration=True, doctype='<!DOCTYPE doc>', standalone=True)
-
-# Write the formatted XML string to a new file
-try:
-    with open('extracted_file.xml', 'w') as file:
-        file.write(pretty_xml)
-except OSError as e:
-    print(f'Error writing extracted XML file: {e}')
-    exit()
+        # Clear the element from memory to free up space
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
