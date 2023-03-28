45013fccd7431b43728c7a7424daa38ddf1205b7
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-28 11:17:57 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,9 +1,15 @@
+import tkinter as tk
+import tkinter.filedialog
+import os
+import chardet
+import concurrent.futures
+
 class FileSearch:
     def __init__(self, window):
         self.window = window
         self.window.title("File Search")
-        self.directory_path = None
-        self.pool = None
+        # Create an executor to run tasks in parallel
+        self.executor = concurrent.futures.ThreadPoolExecutor()
 
         # Create a label and entry widget for the search text
         search_text_label = tk.Label(window, text="Search Text:")
@@ -29,22 +35,20 @@ class FileSearch:
         search_text = self.search_text_entry.get()
         # Search for files in the selected directory
         matching_files = []
-        with multiprocessing.Pool() as pool:
-            self.pool = pool
-            for root, dirs, files in os.walk(self.directory_path):
-                for file in files:
-                    # Check if the search text is in the file name
-                    if search_text.lower() in file.lower():
-                        matching_files.append(os.path.join(root, file))
-                    else:
-                        # Check if the file contains the search text
-                        if file.endswith(('.txt', '.pdf', '.doc', '.docx', '.ppt', '.pptx')):
-                            file_path = os.path.join(root, file)
-                            # Search the file in a separate process
-                            result = self.pool.apply_async(search_file, args=(file_path, search_text))
-                            # Add the file path to the matching files list if it contains the search text
-                            if result.get():
-                                matching_files.append(file_path)
+        for root, dirs, files in os.walk(self.directory_path):
+            for file in files:
+                # Check if the search text is in the file name
+                if search_text.lower() in file.lower():
+                    matching_files.append(os.path.join(root, file))
+                else:
+                    # Check if the file contains the search text
+                    if file.endswith(('.txt', '.pdf', '.doc', '.docx', '.ppt', '.pptx')):
+                        file_path = os.path.join(root, file)
+                        # Search the file in a separate thread
+                        future = self.executor.submit(search_file, file_path, search_text)
+                        # Add the file path to the matching files list if it contains the search text
+                        if future.result():
+                            matching_files.append(file_path)
 
         # Display the matching files in a message box
         if matching_files:
@@ -59,8 +63,18 @@ def search_file(file_path, search_text):
         # Use chardet to detect the encoding of the file
         result = chardet.detect(raw_data)
         encoding = result['encoding']
-        if encoding is not None:
-            text_data = raw_data.decode(encoding, errors='ignore')
-            if search_text in text_data:
-                return True
+        if encoding:
+            try:
+                with open(file_path, 'r', encoding=encoding) as f:
+                    file_content = f.read()
+                    # Check if the search text is in the file
+                    if search_text.lower() in file_content.lower():
+                        return True
+            except:
+                pass
     return False
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = FileSearch(root)
+    root.mainloop()
