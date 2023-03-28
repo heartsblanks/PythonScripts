a7d92726df01afd4f7a8c5eb9f4eca600e08a4df
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-28 11:14:12 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,13 +1,9 @@
-import tkinter as tk
-import tkinter.filedialog
-import os
-import chardet
-import multiprocessing
-
 class FileSearch:
     def __init__(self, window):
         self.window = window
         self.window.title("File Search")
+        self.directory_path = None
+        self.pool = None
 
         # Create a label and entry widget for the search text
         search_text_label = tk.Label(window, text="Search Text:")
@@ -33,25 +29,23 @@ class FileSearch:
         search_text = self.search_text_entry.get()
         # Search for files in the selected directory
         matching_files = []
-        # Create a pool of processes to search the directories in parallel
-        pool = multiprocessing.Pool()
-        for root, dirs, files in os.walk(self.directory_path):
-            for file in files:
-                # Check if the search text is in the file name
-                if search_text.lower() in file.lower():
-                    matching_files.append(os.path.join(root, file))
-                else:
-                    # Check if the file contains the search text
-                    if file.endswith(('.txt', '.pdf', '.doc', '.docx', '.ppt', '.pptx')):
-                        file_path = os.path.join(root, file)
-                        # Search the file in a separate process
-                        result = pool.apply_async(search_file, args=(file_path, search_text))
-                        # Add the file path to the matching files list if it contains the search text
-                        if result.get():
-                            matching_files.append(file_path)
-        # Close the process pool
-        pool.close()
-        pool.join()
+        with multiprocessing.Pool() as pool:
+            self.pool = pool
+            for root, dirs, files in os.walk(self.directory_path):
+                for file in files:
+                    # Check if the search text is in the file name
+                    if search_text.lower() in file.lower():
+                        matching_files.append(os.path.join(root, file))
+                    else:
+                        # Check if the file contains the search text
+                        if file.endswith(('.txt', '.pdf', '.doc', '.docx', '.ppt', '.pptx')):
+                            file_path = os.path.join(root, file)
+                            # Search the file in a separate process
+                            result = self.pool.apply_async(search_file, args=(file_path, search_text))
+                            # Add the file path to the matching files list if it contains the search text
+                            if result.get():
+                                matching_files.append(file_path)
+
         # Display the matching files in a message box
         if matching_files:
             tk.messagebox.showinfo("Search Results", "The following files contain the search text:\n\n" + "\n".join(matching_files))
@@ -65,30 +59,8 @@ def search_file(file_path, search_text):
         # Use chardet to detect the encoding of the file
         result = chardet.detect(raw_data)
         encoding = result['encoding']
-        if encoding is None:
-            # If encoding is None, try a list of common encodings
-            encodings = ['utf-8', 'iso-8859-1', 'cp1252', 'utf-16', 'utf-32']
-            for e in encodings:
-            try:
-                text = raw_data.decode(encoding=e)
-                if search_text in text:
-                    return True
-            except UnicodeDecodeError:
-                pass
-    else:
-        # If encoding is detected, decode the file using that encoding
-        text = raw_data.decode(encoding=encoding)
-        if search_text in text:
-            return True
-return False
-Create the main window
-
-window = tk.Tk()
-
-Create an instance of the FileSearch class
-
-file_search = FileSearch(window)
-
-Run the main event loop
-
-window.mainloop()
+        if encoding is not None:
+            text_data = raw_data.decode(encoding, errors='ignore')
+            if search_text in text_data:
+                return True
+    return False
