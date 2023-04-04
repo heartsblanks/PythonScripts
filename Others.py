3ce019f697d397b14ec31bef89a485686cf9edc3
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-04-04 15:10:50 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,40 +1,16 @@
-import paramiko
-import select
+import os
+import subprocess
 
-# create SSH client
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Define the path to the IIB toolkit installation
+iib_toolkit_path = "/path/to/IIBToolkit"
 
-# connect to remote host
-ssh_client.connect(hostname='your_host', username='your_username', password='your_password')
+# Define the path to the Git plugin archive
+git_plugin_path = "/path/to/git-plugin.tar.gz"
 
-# open SSH channel
-ssh_channel = ssh_client.invoke_shell()
+# Define the command to install the Git plugin
+install_cmd = "{}eclipsec -application org.eclipse.equinox.p2.director \
+  -repository jar:file:{}/tools/plugins/{}!/ \
+  -installIU com.ibm.etools.git.feature.feature.group".format(iib_toolkit_path, iib_toolkit_path, git_plugin_path)
 
-# consume any data before executing mqsireportproperties
-while ssh_channel.recv_ready():
-    ssh_channel.recv(1024)
-
-# execute mqsireportproperties command
-command = 'mqsireportproperties <brokername> -o HTTPConnector -b httplistener -a | awk -F "[= ]" "/port/{print $4}"'
-ssh_channel.send(command + '\n')
-
-# wait until there is data to read
-while not ssh_channel.recv_ready():
-    select.select([ssh_channel], [], [], 1)
-
-# consume any data before the mqsireportproperties output
-while ssh_channel.recv_ready():
-    ssh_channel.recv(1024)
-
-# read the mqsireportproperties output
-output = ''
-while ssh_channel.recv_ready():
-    output += ssh_channel.recv(1024).decode('utf-8')
-
-# print the output
-print(output.strip())
-
-# close SSH channel and client
-ssh_channel.close()
-ssh_client.close()
+# Run the command to install the Git plugin
+subprocess.call(install_cmd, shell=True)
