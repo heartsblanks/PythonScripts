c1602f3215d7d8a9624b0aed32e13bc83c3a3ffe
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-06-26 15:41:44 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,46 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
 
+# Assuming you have a DataFrame called 'df' with columns 'ID' and 'Flow'
+grouped_df = df.groupby('ID')
 
-class Application:
-    def __init__(self, master):
-        self.master = master
+# Create the Tkinter window
+window = tk.Tk()
+window.title("TreeView Example")
 
-        self.treeview = ttk.Treeview(self.master, columns=("Flow"), show="tree")
-        self.treeview.pack()
+# Create the TreeView widget
+tree = ttk.Treeview(window, columns=('Flow'))
 
-        self.data = [
-            ("123", "1023"),
-            ("123", "1123"),
-            ("435", "4035"),
-            ("435", "4135"),
-            ("780", "7080"),
-        ]
+# Custom function to toggle the checkbox state
+def toggle_checkbox(item_id):
+    current_state = tree.item(item_id, 'value')
+    tree.item(item_id, value=not current_state)
 
-        self.populate_treeview()
+# Create a custom checkbox column
+tree.heading('#0', text='ID', anchor='w')
+tree.column('#0', width=100, anchor='w')
 
-    def populate_treeview(self):
-        unique_ids = list(set(item[0] for item in self.data))
-        unique_ids.sort()
+# Insert groups and flows into the TreeView
+for group_id, group_data in grouped_df:
+    # Insert group node with checkbox
+    group_node = tree.insert('', 'end', text=f'{group_id}XXX', value=False)
 
-        for unique_id in unique_ids:
-            group_name = f"{unique_id}XXX"  # Group name is ID followed by 3 X's
-            group_node = self.treeview.insert("", "end", text=group_name, open=True)
+    # Insert flow nodes as children of the group
+    for _, row in group_data.iterrows():
+        tree.insert(group_node, 'end', text=row['Flow'])
 
-            for item in self.data:
-                if item[0] == unique_id:
-                    self.treeview.insert(group_node, "end", text=item[1], values=(item[1]))
+# Bind the checkbox state toggling function to the TreeView
+tree.bind('<Button-1>', lambda event: toggle_checkbox(tree.focus()))
 
-    def toggle_checkbox(self, event):
-        item_id = self.treeview.identify_row(event.y)
-        if item_id:
-            tags = self.treeview.item(item_id, "tags")
-            if "item" in tags:
-                current_value = self.treeview.item(item_id, "text")
-                new_value = f"[x] {current_value}" if "[x]" not in current_value else current_value.replace("[x]", "")
-                self.treeview.item(item_id, text=new_value)
+# Pack the TreeView widget
+tree.pack()
 
-
-root = tk.Tk()
-app = Application(root)
-root.mainloop()
+# Start the Tkinter event loop
+window.mainloop()
