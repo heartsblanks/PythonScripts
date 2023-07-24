0f8c343c092e4f456deed49e87617771a7931d3c
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-07-24 14:03:53 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -25,15 +25,22 @@ for node in top_level_nodes:
         start_nodes.append(node_id)
 
 # Sort start nodes based on their Y location value
-start_nodes.sort(key=lambda node_id: int(top_level_nodes[node_id]['@location'].split(',')[1]))
+start_nodes.sort(key=lambda node_id: int([node['@location'].split(',')[1] for node in top_level_nodes if node['@xmi:id'] == node_id][0]))
 
 # Assign unique locations to start nodes
-y_increment = 20
+y_increment = 300
+updated_nodes = set()
 for i, start_node_id in enumerate(start_nodes):
+    if i == 0:
+        new_location = "0,20"
+        y_location = 20
+    else:
+        new_location = f"0,{y_location + i * y_increment}"
+
     start_node = next(node for node in top_level_nodes if node['@xmi:id'] == start_node_id)
-    current_location = start_node['@location']
-    new_location = increment_location(current_location, 0, y_increment * i)
-    start_node['@location'] = new_location
+    if start_node_id not in updated_nodes:
+        start_node['@location'] = new_location
+        updated_nodes.add(start_node_id)
 
 # Follow connections and assign new locations to connected nodes
 for start_node_id in start_nodes:
@@ -47,7 +54,7 @@ for start_node_id in start_nodes:
         current_node = next(node for node in top_level_nodes if node['@xmi:id'] == current_node_id)
 
         # If current node exists, assign a new location to its connected nodes
-        if current_node:
+        if current_node_id not in updated_nodes:
             current_location = current_node['@location']
             new_location = increment_location(current_location, 50, 0)
 
@@ -73,6 +80,8 @@ for start_node_id in start_nodes:
                         # Store the target node and its Y increment in the dictionary
                         target_node_positions[target_node_id] = -50 + (i - 1) * 50
 
+            updated_nodes.add(current_node_id)
+
             # Move to the first target node for the next iteration
             if target_node_ids:
                 current_node_id = target_node_ids[0]
@@ -83,11 +92,12 @@ for start_node_id in start_nodes:
     # Update the Y increments for all target nodes connected to the same node
     for target_node_id in target_node_positions:
         for target_node in top_level_nodes:
-            if target_node['@xmi:id'] == target_node_id:
+            if target_node['@xmi:id'] == target_node_id and target_node_id not in updated_nodes:
                 current_location = target_node['@location']
                 x, y = map(int, current_location.split(','))
                 new_y = y + target_node_positions[target_node_id]
                 target_node['@location'] = f"{x},{new_y}"
+                updated_nodes.add(target_node_id)
 
 # Convert the updated data back to XML format
 updated_xml = xmltodict.unparse(parsed_data, pretty=True)
