e64a20550125a4707e0124d40aeda2fbe9b142da
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-08-10 11:17:08 +0200

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,19 +1,14 @@
-Certainly! Here's a simple example code snippet that demonstrates how to use the `python-nexus` library to check the connection to a Nexus repository:
+import requests
 
-```python
-from nexus import NexusClient
-
-# Create a Nexus client instance
-nexus_client = NexusClient(base_url="https://your.repository.url", username="your_username", password="your_password")
+repository_url = "https://your.repository.url"
+username = "your_username"
+password = "your_password"
 
 try:
-    # Attempt to authenticate and check the connection
-    nexus_client.authenticate()
-    print("Connection to Nexus repository successful!")
-except Exception as e:
+    response = requests.get(repository_url, auth=(username, password))
+    if response.status_code == 200:
+        print("Connection to Nexus repository successful!")
+    else:
+        print(f"Connection to Nexus repository failed with status code: {response.status_code}")
+except requests.RequestException as e:
     print(f"Connection to Nexus repository failed: {e}")
-```
-
-Replace `"https://your.repository.url"` with the actual URL of your Nexus repository, and provide your Nexus username and password in the `username` and `password` parameters.
-
-This code will try to authenticate with the Nexus repository using the provided credentials. If the connection is successful, it will print a success message. If there's an issue with the connection, it will print an error message with the details of the exception.
