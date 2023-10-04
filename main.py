dcf4452149075d85aecbddbb1e9a1d047c9cd91e
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-04 13:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import time
 
 def simulate_migration(stage, progress, stage_canvas, stage_text):
@@ -37,9 +38,9 @@ for stage in range(1, 11):
     stage_text = tk.StringVar()
     stage_label = tk.Label(stage_canvas, textvariable=stage_text, anchor=tk.W)
     stage_label.place(x=10, y=10)
-    
+
     stage_progress[stage] = tk.IntVar()
-    stage_bar = tk.Progressbar(stage_canvas, mode="determinate", length=300, variable=stage_progress[stage])
+    stage_bar = ttk.Progressbar(stage_canvas, mode="determinate", length=300, variable=stage_progress[stage])
     stage_bar.place(x=10, y=20)
 
     stage_canvases[stage] = stage_canvas
