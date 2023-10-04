8b89444e979bb13acb50cdbac5b035eb69141f6d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-04 14:30:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,45 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import time
 
-def simulate_migration(stage, progress_bar, stage_text, percentage_text, status_text):
+def simulate_migration(stage, stage_text, percentage_text, status_text):
     for i in range(101):
         percentage_text.set(f"{i}%")
         status_text.set("Completed" if i == 100 else "In Progress")
-        progress_bar["value"] = i
+        stage_canvases[stage].itemconfig(stage_bars[stage], value=i)
         root.update_idletasks()
         time.sleep(0.01)
 
     if i == 100:
         status_text.set("Completed")
-        progress_bar["style"] = "green.Horizontal.TProgressbar"
+        stage_canvases[stage].itemconfig(stage_bars[stage], fill="green")
     else:
         status_text.set("Error")
-        progress_bar["style"] = "red.Horizontal.TProgressbar"
+        stage_canvases[stage].itemconfig(stage_bars[stage], fill="red")
 
 def migrate(stage):
     if stage > 10:
         return
-    simulate_migration(stage, progress_bars[stage], stage_texts[stage], percentage_texts[stage], status_texts[stage])
+
+    show_progress_bar(stage)
+    simulate_migration(stage, stage_texts[stage], percentage_texts[stage], status_texts[stage])
+    hide_progress_bar(stage)
+
     root.after(10, migrate, stage + 1)
+
+def show_progress_bar(stage):
+    stage_canvases[stage].grid(row=0, column=3, padx=10)
+
+def hide_progress_bar(stage):
+    stage_canvases[stage].grid_remove()
 
 root = tk.Tk()
 root.title("Migration Progress")
 
-# Create dictionaries to store progress bars and text elements
-progress_bars = {}
+stage_canvases = {}
 stage_texts = {}
 percentage_texts = {}
 status_texts = {}
+stage_bars = {}
 
 for stage in range(1, 11):
     stage_frame = tk.Frame(root)
@@ -48,10 +58,12 @@ for stage in range(1, 11):
     status_label = tk.Label(stage_frame, textvariable=status_text, anchor=tk.W)
     status_label.grid(row=0, column=2, padx=10)
 
-    progress_bar = ttk.Progressbar(stage_frame, orient="horizontal", length=320, mode="determinate")
-    progress_bar.grid(row=0, column=3, padx=10)
+    stage_canvas = tk.Canvas(stage_frame, width=320, height=40)
+    stage_canvas.grid(row=0, column=3, padx=10)
 
-    progress_bars[stage] = progress_bar
+    stage_bar = ttk.Progressbar(stage_canvas, orient="horizontal", mode="determinate", length=320)
+    stage_bars[stage] = stage_bar
+    stage_canvases[stage] = stage_canvas
     stage_texts[stage] = stage_text
     percentage_texts[stage] = percentage_text
     status_texts[stage] = status_text
