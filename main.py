3f15e63246c0ba5a5eef2028e9285c93fbeb18d7
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-04 12:16:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,59 @@
 import sys
-import math
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsRectItem
-from PyQt5.QtGui import QColor, QBrush, QPen
-from PyQt5.QtCore import Qt, QPointF, QLineF
+import subprocess
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+import PyQt5.QtWidgets as QtWidgets
 
-class StageItem(QGraphicsRectItem):
-    def __init__(self, name, x, y, width, height):
-        super().__init__(x, y, width, height)
-        self.name = name
-        self.setBrush(QBrush(Qt.gray))
-        self.setPen(QPen(Qt.black))
-        self.successful = False
-        self.progress = 0  # Initialize progress to 0
+class PackageInstaller(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+    
+    def init_ui(self):
+        self.setWindowTitle("Package Installer")
+        self.setGeometry(100, 100, 600, 400)
 
-    def set_success(self, success):
-        self.successful = success
-        self.setBrush(QBrush(Qt.green if success else Qt.red))
+        # List widget to display package installation status
+        self.package_list = QtWidgets.QListWidget(self)
+        self.package_list.setGeometry(50, 50, 300, 200)
 
-    def set_progress(self, progress):
-        self.progress = progress
-        self.update()  # Call update to redraw the stage with updated progress
+        # Progress bar
+        self.progress_bar = QtWidgets.QProgressBar(self)
+        self.progress_bar.setGeometry(50, 270, 300, 20)
 
-    def paint(self, painter, option, widget=None):
-        super().paint(painter, option, widget)
-        # Draw a progress bar within the stage
-        progress_rect = self.rect()
-        progress_rect.setWidth(self.progress * self.rect().width() / 100)  # Adjust width based on progress
-        painter.fillRect(progress_rect, QBrush(Qt.green))
+        # Start installation button
+        self.start_button = QtWidgets.QPushButton("Start Installation", self)
+        self.start_button.setGeometry(50, 310, 120, 30)
+        self.start_button.clicked.connect(self.install_packages)
 
-def draw_arrow(scene, start_item, end_item):
-    # Rest of the code remains the same...
+    def install_packages(self):
+        packages_to_install = ["package1", "package2", "package3"]  # Replace with your package list
+        total_packages = len(packages_to_install)
+        successful_installs = 0
+
+        for package in packages_to_install:
+            try:
+                # Execute pip install command
+                result = subprocess.run(["pip", "install", package], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+
+                # Update list widget with success status
+                item = QtWidgets.QListWidgetItem(f"{package} - Installed", self.package_list)
+                item.setIcon(QtGui.QIcon("green_tick_icon.png"))  # Replace with your green tick icon path
+                successful_installs += 1
+
+            except subprocess.CalledProcessError as e:
+                # Update list widget with failure status
+                item = QtWidgets.QListWidgetItem(f"{package} - Failed to install", self.package_list)
+                item.setIcon(QtGui.QIcon("red_x_icon.png"))  # Replace with your red X icon path
+
+            # Update progress bar
+            progress_percent = (successful_installs / total_packages) * 100
+            self.progress_bar.setValue(progress_percent)
 
 def main():
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-    scene = QGraphicsScene()
-    view = QGraphicsView(scene)
-
-    stages = [
-        StageItem("Stage 1", 10, 10, 100, 50),
-        StageItem("Stage 2", 150, 10, 100, 50),
-        # Add more stages as needed
-    ]
-
-    for stage in stages:
-        scene.addItem(stage)
-
-    # Simulate success/failure of stages
-    stages[0].set_success(True)
-    stages[1].set_success(False)
-
-    # Update progress for demonstration (you can replace this with your actual progress updates)
-    stages[0].set_progress(30)
-    stages[1].set_progress(70)
-
-    # Draw arrows connecting stages
-    draw_arrow(scene, stages[0], stages[1])
-    # Add more arrows as needed
-
-    view.show()
+    app = QtWidgets.QApplication(sys.argv)
+    window = PackageInstaller()
+    window.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
