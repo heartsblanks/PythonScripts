5df889ffaa467a771b58ceb8209b45088d54dcf1
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-04 12:49:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
-import time
 import threading
+import time
 
 def migrate():
     def simulate_migration(stage):
-        current_progress_bar = stage_progress_bars[stage]
-        current_progress_bar.config(value=0)
-        current_progress_bar.step(1)
-        stage_label.config(text=f"Stage {stage}")
+        canvas.itemconfig(stage_text, text=f"Stage {stage}")
         for i in range(101):
+            canvas.coords(stage_bar, 10, 10, 10 + i, 30)
+            canvas.update()
             time.sleep(0.01)
-            current_progress_bar.step(1)
-            current_progress_bar["text"] = f"Stage {stage}"
-            root.update_idletasks()
-        current_progress_bar["text"] = ""  # Clear the text when the stage is completed
         if stage == 10:
-            stage_label.config(text="Migration Completed", fg="green")
+            canvas.itemconfig(stage_text, text="Migration Completed", fill="green")
         else:
-            stage_label.config(text=f"Error in Stage {stage}", fg="red")
+            canvas.itemconfig(stage_text, text=f"Error in Stage {stage}", fill="red")
 
     for stage in range(1, 11):
         t = threading.Thread(target=simulate_migration, args=(stage,))
@@ -27,14 +21,11 @@ def migrate():
 root = tk.Tk()
 root.title("Migration Progress")
 
-stage_progress_bars = []
-for _ in range(11):
-    progress_bar = ttk.Progressbar(root, mode="determinate", length=300)
-    stage_progress_bars.append(progress_bar)
-    progress_bar.pack(pady=5)
+canvas = tk.Canvas(root, width=320, height=50)
+canvas.pack(pady=10)
 
-stage_label = tk.Label(root, text="", pady=10)
-stage_label.pack()
+stage_text = canvas.create_text(10, 35, anchor=tk.W, text="", font=("Helvetica", 10))
+stage_bar = canvas.create_rectangle(10, 10, 10, 30, fill="green")
 
 migrate_button = tk.Button(root, text="Migrate", command=migrate)
 migrate_button.pack(pady=10)
