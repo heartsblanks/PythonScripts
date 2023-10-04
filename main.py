7ef76979bba9a337c9d0b74183e8c357b0d8b7aa
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-04 12:58:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 import tkinter as tk
 import time
 
+def simulate_migration(stage, progress, stage_canvas, stage_text):
+    for i in range(101):
+        progress.set(i)
+        stage_canvas.coords(stage_bar, 10, 10, 10 + i, 30)
+        stage_text.set(f"Stage {stage} - {i}%")
+        root.update_idletasks()
+        time.sleep(0.01)
+
+    if progress.get() == 100:
+        stage_text.set(f"Stage {stage} - Completed")
+        stage_canvas.itemconfig(stage_bar, fill="green")
+    else:
+        stage_text.set(f"Stage {stage} - Error")
+        stage_canvas.itemconfig(stage_bar, fill="red")
+
 def migrate():
-    def simulate_migration(stage):
-        progress = 0
-        stage_canvas = stage_canvases[stage]
-        stage_text = stage_texts[stage]
-
-        for i in range(101):
-            progress = i
-            stage_canvas.coords(stage_bar, 10, 10, 10 + progress, 30)
-            stage_canvas.itemconfig(stage_text, text=f"Stage {stage} - {progress}%")
-            stage_canvas.update()
-            time.sleep(0.01)
-
-        if progress == 100:
-            stage_canvas.itemconfig(stage_text, text=f"Stage {stage} - Completed", fill="green")
-        else:
-            stage_canvas.itemconfig(stage_text, text=f"Stage {stage} - Error", fill="red")
-
-        # Check if this is the last stage before trying to access the next stage
-        if stage < 10:
-            simulate_migration(stage + 1)
+    for stage in range(1, 11):
+        simulate_migration(stage, stage_progress[stage], stage_canvases[stage], stage_texts[stage])
 
 root = tk.Tk()
 root.title("Migration Progress")
 
-stage_canvases = []
-stage_texts = []
+stage_canvases = {}
+stage_texts = {}
+stage_progress = {}
 
-for stage in range(11):
+for stage in range(1, 11):
     stage_frame = tk.Frame(root)
     stage_frame.pack(pady=10)
 
     stage_canvas = tk.Canvas(stage_frame, width=320, height=40)
     stage_canvas.pack()
 
-    stage_text = stage_canvas.create_text(10, 20, anchor=tk.W, text=f"Stage {stage} - 0%")
-    stage_bar = stage_canvas.create_rectangle(10, 10, 10, 30, fill="green")
+    stage_text = tk.StringVar()
+    stage_label = tk.Label(stage_canvas, textvariable=stage_text, anchor=tk.W)
+    stage_label.place(x=10, y=10)
+    
+    stage_progress[stage] = tk.IntVar()
+    stage_bar = tk.Progressbar(stage_canvas, mode="determinate", length=300, variable=stage_progress[stage])
+    stage_bar.place(x=10, y=20)
 
-    stage_canvases.append(stage_canvas)
-    stage_texts.append(stage_text)
+    stage_canvases[stage] = stage_canvas
+    stage_texts[stage] = stage_text
 
-migrate_button = tk.Button(root, text="Migrate", command=lambda: simulate_migration(1))
+migrate_button = tk.Button(root, text="Migrate", command=migrate)
 migrate_button.pack(pady=10)
 
 root.mainloop()
