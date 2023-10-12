8b8e05116d8e0d0373c683e5cee1ca9ef8d9c30a
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-12 11:55:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import os
+import xmltodict
 
-def get_tool_install_directory_and_version(tool_name, install_directory):
-    tool_install_directory = None
-    latest_version = None
+def update_xml_with_missing_elements(source_file, dest_file, parent_tags):
+    # Read and parse the source XML
+    with open(source_file, "r") as source_xml:
+        source_data = source_xml.read()
+    source_dict = xmltodict.parse(source_data)
 
-    for folder in os.listdir(install_directory):
-        if tool_name.lower() in folder.lower() and os.path.isdir(os.path.join(install_directory, folder)):
-            tool_install_directory = os.path.join(install_directory, folder)
-            version_folders = sorted([subfolder for subfolder in os.listdir(tool_install_directory) if os.path.isdir(os.path.join(tool_install_directory, subfolder))], reverse=True)
-            
-            if version_folders:
-                latest_version = version_folders[0]
+    # Read and parse the destination XML
+    with open(dest_file, "r") as dest_xml:
+        dest_data = dest_xml.read()
+    dest_dict = xmltodict.parse(dest_data)
 
-    return tool_install_directory, latest_version
+    # Traverse the dictionaries using the specified parent tags
+    source_parent = source_dict
+    dest_parent = dest_dict
+    for tag in parent_tags:
+        source_parent = source_parent.get(tag, {})
+        dest_parent = dest_parent.get(tag, {})
+
+    # Create a set of existing element names in the destination XML
+    dest_element_names = {elem['@id'] for elem in dest_parent if isinstance(dest_parent, list) for elem in dest_parent}
+
+    # Check and add missing elements to the destination XML
+    for source_element in source_parent:
+        if source_element['@id'] not in dest_element_names:
+            dest_parent.append(source_element)
+
+    # Write the updated destination XML back to the file
+    with open(dest_file, "w") as dest_xml:
+        dest_xml.write(xmltodict.unparse(dest_dict, pretty=True))
 
 # Example usage:
-tool_name = "ACE"  # Change this to the tool you're looking for
-install_directory = "C:\\Program Files\\IBM"  # Change this to the actual install directory
-
-install_dir, latest_version = get_tool_install_directory_and_version(tool_name, install_directory)
-
-if install_dir:
-    if latest_version:
-        print(f"Install directory for {tool_name} is: {os.path.join(install_dir, latest_version)}")
-    else:
-        print(f"Install directory for {tool_name} is: {install_dir}")
-else:
-    print(f"{tool_name} not found in {install_directory}")
+source_xml_file = "path/to/source.xml"
+dest_xml_file = "path/to/destination.xml"
+parent_tags = ["project", "dependencies"]  # Specify the tag hierarchy
+update_xml_with_missing_elements(source_xml_file, dest_xml_file, parent_tags)
