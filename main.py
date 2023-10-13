cc6b0b964d935c0b8626868c88681192bd36b924
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-13 13:13:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-import xmltodict
+import subprocess
+import tkinter as tk
 
-def update_xml_with_missing_elements(source_file, dest_file, parent_tags):
-    # Read and parse the source XML
-    with open(source_file, "r") as source_xml:
-        source_data = source_xml.read()
-    source_dict = xmltodict.parse(source_data)
+def run_maven_command():
+    # Replace 'your_maven_command' with your actual Maven command
+    maven_command = 'your_maven_command'
+    
+    # Create a new subprocess and open a pipe to its standard output
+    process = subprocess.Popen(maven_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    
+    # Create a Tkinter window
+    window = tk.Tk()
+    window.title("Maven Command Output")
+    
+    # Create a Text widget to display the command output
+    output_text = tk.Text(window)
+    output_text.pack()
+    
+    # Create a function to update the Text widget in real-time
+    def update_output():
+        line = process.stdout.readline()
+        if line:
+            output_text.insert(tk.END, line)
+            output_text.see(tk.END)  # Auto-scroll to the end of the Text widget
+            window.after(10, update_output)  # Schedule the function to run again
+        else:
+            # Command has finished, disable the button
+            run_button.config(state=tk.DISABLED)
+    
+    # Create a button to trigger the Maven command
+    run_button = tk.Button(window, text="Run Maven Command", command=update_output)
+    run_button.pack()
+    
+    update_output()  # Start the real-time update
+    
+    window.mainloop()
 
-    # Read and parse the destination XML
-    with open(dest_file, "r") as dest_xml:
-        dest_data = dest_xml.read()
-    dest_dict = xmltodict.parse(dest_data)
-
-    # Traverse the dictionaries using the specified parent tags
-    source_parent = source_dict
-    dest_parent = dest_dict
-
-    for tag in parent_tags:
-        source_parent = source_parent.get(tag, {})
-        dest_parent = dest_parent.get(tag, {})
-
-    if isinstance(dest_parent, list):
-        # Create a set of existing elements in the destination XML
-        dest_element_set = set(xmltodict.PrettyDict(element) for element in dest_parent)
-
-        # Check and add missing elements to the destination XML
-        for source_element in source_parent:
-            if xmltodict.PrettyDict(source_element) not in dest_element_set:
-                dest_parent.append(source_element)
-    elif isinstance(dest_parent, dict):
-        # In this case, we assume there's only one element under the parent tag
-        source_element = source_parent
-        dest_element = dest_parent
-        if xmltodict.PrettyDict(source_element) != xmltodict.PrettyDict(dest_element):
-            dest_parent = source_element
-
-    # Write the updated destination XML back to the file
-    with open(dest_file, "w") as dest_xml:
-        dest_xml.write(xmltodict.unparse(dest_dict, pretty=True))
-
-# Example usage:
-source_xml_file = "path/to/source.xml"
-dest_xml_file = "path/to/destination.xml"
-parent_tags = ["project", "dependencies"]  # Specify the tag hierarchy
-update_xml_with_missing_elements(source_xml_file, dest_xml_file, parent_tags)
+# Run the Maven command when the script starts
+run_maven_command()
