dd1f7d84a64e4369e38f35f25fbd6e71a28f4dbb
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-16 15:12:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
-import os
-import subprocess
-import ast
-import concurrent.futures
+import openpyxl
+from openpyxl.styles import PatternFill
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
-def get_imports(file_path):
-    with open(file_path, 'r') as file:
-        tree = ast.parse(file.read())
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            for n in node.names:
-                yield n.name
-        if isinstance(node, ast.ImportFrom):
-            yield node.module
+# Parse the JUnit XML report
+tree = ET.parse('junit_report.xml')
+root = tree.getroot()
 
-def is_package_installed(package_name):
-    try:
-        __import__(package_name)
-        return True
-    except ImportError:
-        return False
+# Load an existing Excel workbook or create a new one if it doesn't exist
+try:
+    workbook = openpyxl.load_workbook('test_report.xlsx')
+    sheet = workbook.active
+except FileNotFoundError:
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "JUnit Test Report"
+    # Add headers for each column
+    sheet.append(['Package', 'Class', 'Test Case', 'Status', 'Error/Failure Message', 'Previous Status', 'Updated Date', 'Time Taken'])
 
-def install_package(package_name):
-    subprocess.run(["pip", "install", package_name])
+# Create fill patterns for different status colors
+passed_fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+failed_fill = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid")
+error_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
 
-def process_python_file(file_path):
-    for item in get_imports(file_path):
-        # Check if it's a package, and if not, try to install it
-        if not is_package_installed(item):
-            print(f"{item} is not installed. Installing...")
-            install_package(item)
+# Iterate through the XML and populate/update the Excel sheet
+for testcase in root.iter('testcase'):
+    package, classname = testcase.attrib['classname'].rsplit('.', 1)
+    test_case_name = testcase.attrib['name']
+    failure_element = testcase.find('failure')
+    error_element = testcase.find('error')
+    
+    # Check if the test case already exists in the Excel sheet
+    test_case_exists = False
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=3):
+        if [cell.value for cell in row] == [package, classname, test_case_name]:
+            test_case_exists = True
+            status_cell = row[3]
+            error_message_cell = row[4]
+            previous_status_cell = row[5]
+            updated_date_cell = row[6]
+            time_taken_cell = row[7]
+            break
 
-directory_path = '/your/directory/path'
+    # If the test case exists, update its status, error message, and date
+    if test_case_exists:
+        status_cell.value = "Failed" if failure_element is not None else ("Error" if error_element is not None else "Passed")
+        error_message_cell.value = failure_element.attrib.get('message') if failure_element is not None else (error_element.attrib.get('message') if error_element is not None else '')
+        previous_status_cell.value = previous_status_cell.value if previous_status_cell.value else status_cell.value
+        updated_date_cell.value = datetime.now()
+        time_taken_cell.value = testcase.attrib.get('time', '')
 
-# Specify the number of concurrent workers
-num_workers = 4
+        # Apply conditional formatting for the "Status" column
+        if status_cell.value == "Failed":
+            status_cell.fill = failed_fill
+        elif status_cell.value == "Error":
+            status_cell.fill = error_fill
+        else:
+            status_cell.fill = passed_fill
+    # If the test case is new, add it to the Excel sheet
+    else:
+        status = "Failed" if failure_element is not None else ("Error" if error_element is not None else "Passed")
+        error_message = failure_element.attrib.get('message') if failure_element is not None else (error_element.attrib.get('message') if error_element is not None else '')
+        updated_date = datetime.now()
+        time_taken = testcase.attrib.get('time', '')
+        # Add a row for the test case and its details
+        row = [package, classname, test_case_name, status, error_message, status, updated_date, time_taken]
+        sheet.append(row)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-    for root, _, files in os.walk(directory_path):
-        for file in files:
-            if file.endswith('.py'):
-                file_path = os.path.join(root, file)
-                executor.submit(process_python_file, file_path)
+# Save the Excel file
+workbook.save('test_report.xlsx')
