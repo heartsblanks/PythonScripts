270dfcd36e15fc058d3f9b273f360a00eb520a4a
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-18 14:09:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,45 @@
-import subprocess
 import tkinter as tk
+from maven import execute_maven_command
 
-def run_maven_command(maven_command, master):
-    # Create a new subprocess and open a pipe to its standard output
-    process = subprocess.Popen(maven_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    
-    # Create a Tkinter window
-    window = tk.Toplevel(master)
-    window.title("Maven Command Output")
-    
-    # Create a Text widget to display the command output with monospaced font
-    output_text = tk.Text(window, wrap=tk.WORD, font=("Courier New", 10))
-    output_text.pack()
-    
-    # Configure background and foreground colors
-    output_text.config(bg="black", fg="white")
-    
-    # Create a function to update the Text widget in real-time
-    def update_output():
-        line = process.stdout.readline()
-        if line:
-            output_text.insert(tk.END, line)
-            output_text.see(tk.END)  # Auto-scroll to the end of the Text widget
-            window.after(10, update_output)  # Schedule the function to run again
-        else:
-            # Command has finished, disable the button
-            run_button.config(state=tk.DISABLED)
-            
-            # Wait for the process to complete and get the exit code
-            process.wait()
-            return_code = process.returncode
-            
-            if return_code == 0:
-                window.destroy()  # Close the window if the command was successful
+def on_command_completion(result, return_code):
+    if result:
+        print(f"Maven command completed successfully with return code {return_code}")
+    else:
+        print(f"Maven command failed with return code {return_code}")
 
-    # Create a button to trigger the Maven command
-    run_button = tk.Button(window, text="Run Maven Command", command=update_output)
-    run_button.pack()
-    
-    update_output()  # Start the real-time update
-
-# Example usage:
 root = tk.Tk()
 root.title("Maven Command Runner")
 
 # Replace 'your_maven_command' with the actual Maven command
-maven_command = 'your_maven_command'
+maven_command = 'your_maven_command'  # Replace with your actual Maven command
 
-# Pass the Maven command and the master window to the function
-run_maven_command(maven_command, root)
+execute_maven_command(maven_command, root, on_command_completion)
 
-# Start the Tkinter main loop
 root.mainloop()
+
+import subprocess
+import tkinter as tk
+
+def execute_maven_command(maven_command, master, callback):
+    process = subprocess.Popen(maven_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    window = tk.Toplevel(master)
+    window.title("Maven Command Output")
+
+    output_text = tk.Text(window, wrap=tk.WORD, font=("Courier New", 10))
+    output_text.pack()
+    output_text.config(bg="black", fg="white")
+
+    def update_output():
+        line = process.stdout.readline()
+        if line:
+            output_text.insert(tk.END, line)
+            output_text.see(tk.END)
+            window.after(1, update_output)
+        else:
+            process.wait()
+            return_code = process.returncode
+            window.destroy()
+            callback(return_code == 0, return_code)
+
+    run_button = tk.Button(window, text="Run Maven Command", command=update_output)
+    run_button.pack()
