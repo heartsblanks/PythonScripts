61066df29fdc67debb049abd1bb7f6bff2216745
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-18 15:32:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import subprocess
 import tkinter as tk
+import threading
 
-# Create a tkinter window and text box
+def execute_maven_command():
+    command = "mvn install"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    for line in process.stdout:
+        text_box.insert(tk.END, line)
+        text_box.see(tk.END)
+        root.update_idletasks()
+    process.wait()
+
+def display_output():
+    thread = threading.Thread(target=execute_maven_command)
+    thread.start()
+
 root = tk.Tk()
 text_box = tk.Text(root)
 text_box.pack()
 
-# Command to execute
-command = "mvn install"
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-
-# Function to display real-time output in the tkinter text box
-def display_output():
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            text_box.insert(tk.END, output)
-            text_box.see(tk.END)
-        root.update_idletasks()
-
-# Wait for the command to complete and then execute the next lines of code
-process.wait()
-
-# Now you can proceed with the next lines of code
-text_box.insert(tk.END, "Maven command has completed.")
-root.update_idletasks()
-
-# Start the real-time output display function
-display_output()
+tk.Button(root, text="Run Maven Command", command=display_output).pack()
 
 root.mainloop()
