9ba3d3303668f8a5a0722c2f10ffe9e060495663
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-18 12:18:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,49 @@
-import openpyxl
-from openpyxl.styles import PatternFill
-import xml.etree.ElementTree as ET
-from datetime import datetime
+import subprocess
+import tkinter as tk
 
-# Parse the JUnit XML report
-tree = ET.parse('junit_report.xml')
-root = tree.getroot()
-
-# Load an existing Excel workbook or create a new one if it doesn't exist
-try:
-    workbook = openpyxl.load_workbook('test_report.xlsx')
-    sheet = workbook.active
-except FileNotFoundError:
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-    sheet.title = "JUnit Test Report"
-    # Add headers for each column
-    sheet.append(['Package', 'Class', 'Test Case', 'Status', 'Error/Failure Message', 'Previous Status', 'Updated Date', 'Time Taken'])
-
-# Create fill patterns for different status colors
-passed_fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
-failed_fill = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid")
-error_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
-
-# Iterate through the XML and populate/update the Excel sheet
-for testcase in root.iter('testcase'):
-    package, classname = testcase.attrib['classname'].rsplit('.', 1)
-    test_case_name = testcase.attrib['name']
-    failure_element = testcase.find('failure')
-    error_element = testcase.find('error')
+def run_maven_command():
+    # Replace 'your_maven_command' with your actual Maven command
+    maven_command = 'your_maven_command'
     
-    # Check if the test case already exists in the Excel sheet
-    test_case_exists = False
-    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=3):
-        if [cell.value for cell in row] == [package, classname, test_case_name]:
-            test_case_exists = True
-            status_cell = row[3]
-            error_message_cell = row[4]
-            previous_status_cell = row[5]
-            updated_date_cell = row[6]
-            time_taken_cell = row[7]
-            break
-
-    # If the test case exists, update its status, error message, and date
-    if test_case_exists:
-        new_status = "Failed" if failure_element is not None else ("Error" if error_element is not None else "Passed")
-        if new_status != status_cell.value:
-            previous_status_cell.value = status_cell.value
-            status_cell.value = new_status
-        error_message_cell.value = failure_element.attrib.get('message') if failure_element is not None else (error_element.attrib.get('message') if error_element is not None else '')
-        updated_date_cell.value = datetime.now()
-        time_taken_cell.value = testcase.attrib.get('time', '')
-
-        # Reapply conditional formatting for the "Status" column
-        if status_cell.value == "Failed":
-            status_cell.fill = failed_fill
-        elif status_cell.value == "Error":
-            status_cell.fill = error_fill
+    # Create a new subprocess and open a pipe to its standard output
+    process = subprocess.Popen(maven_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    
+    # Create a Tkinter window
+    window = tk.Tk()
+    window.title("Maven Command Output")
+    
+    # Create a Text widget to display the command output with monospaced font
+    output_text = tk.Text(window, wrap=tk.WORD, font=("Courier New", 10))
+    output_text.pack()
+    
+    # Configure background and foreground colors
+    output_text.config(bg="black", fg="white")
+    
+    # Create a function to update the Text widget in real-time
+    def update_output():
+        line = process.stdout.readline()
+        if line:
+            output_text.insert(tk.END, line)
+            output_text.see(tk.END)  # Auto-scroll to the end of the Text widget
+            window.after(10, update_output)  # Schedule the function to run again
         else:
-            status_cell.fill = passed_fill
-    # If the test case is new, add it to the Excel sheet
-    else:
-        status = "Failed" if failure_element is not None else ("Error" if error_element is not None else "Passed")
-        error_message = failure_element.attrib.get('message') if failure_element is not None else (error_element.attrib.get('message') if error_element is not None else '')
-        updated_date = datetime.now()
-        time_taken = testcase.attrib.get('time', '')
-        # Add a row for the test case and its details
-        row = [package, classname, test_case_name, status, error_message, status, updated_date, time_taken]
-        sheet.append(row)
+            # Command has finished, disable the button
+            run_button.config(state=tk.DISABLED)
+            
+            # Wait for the process to complete and get the exit code
+            process.wait()
+            return_code = process.returncode
+            
+            if return_code == 0:
+                window.destroy()  # Close the window if the command was successful
 
-# Save the Excel file
-workbook.save('test_report.xlsx')
+    # Create a button to trigger the Maven command
+    run_button = tk.Button(window, text="Run Maven Command", command=update_output)
+    run_button.pack()
+    
+    update_output()  # Start the real-time update
+    
+    window.mainloop()
+
+# Run the Maven command when the script starts
+run_maven_command()
