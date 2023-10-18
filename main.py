b8bfc79c1b5e2698b79ff532072f02bb3ae37c6d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-18 14:14:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ def on_command_completion(result, return_code):
 root = tk.Tk()
 root.title("Maven Command Runner")
 
+# Create a frame within the main window to display the Maven command output
+output_frame = tk.Frame(root)
+output_frame.pack()
+
 # Replace 'your_maven_command' with the actual Maven command
 maven_command = 'your_maven_command'  # Replace with your actual Maven command
 
-execute_maven_command(maven_command, root, on_command_completion)
+print("Before executing Maven command")
+
+execute_maven_command(maven_command, output_frame, on_command_completion)
+
+print("After executing Maven command")
 
 root.mainloop()
+
 
 import subprocess
 import tkinter as tk
 
-def execute_maven_command(maven_command, master, callback):
+def execute_maven_command(maven_command, frame, callback):
     process = subprocess.Popen(maven_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    window = tk.Toplevel(master)
-    window.title("Maven Command Output")
 
-    output_text = tk.Text(window, wrap=tk.WORD, font=("Courier New", 10))
+    output_text = tk.Text(frame, wrap=tk.WORD, font=("Courier New", 10)
     output_text.pack()
     output_text.config(bg="black", fg="white")
 
@@ -34,12 +41,11 @@ def execute_maven_command(maven_command, master, callback):
         if line:
             output_text.insert(tk.END, line)
             output_text.see(tk.END)
-            window.after(1, update_output)
+            frame.after(1, update_output)
         else:
             process.wait()
             return_code = process.returncode
-            window.destroy()
             callback(return_code == 0, return_code)
+            output_text.config(state=tk.DISABLED)
 
-    run_button = tk.Button(window, text="Run Maven Command", command=update_output)
-    run_button.pack()
+    update_output()
