390244887353d028531d2598adc58bf23dffe7fe
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-30 11:21:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from ttkwidgets import ScrolledFrame
 
 def add_stage():
     global stage_count
@@ -7,29 +7,14 @@ def add_stage():
         stage_label = tk.Label(inner_frame, text=f"Stage {stage_count + 1}")
         stage_label.grid(row=stage_count, column=0, sticky='w')
         stage_count += 1
-        update_canvas_scrollregion()
-
-def update_canvas_scrollregion():
-    canvas.update_idletasks()
-    canvas.config(scrollregion=canvas.bbox("all"))
 
 root = tk.Tk()
 root.title("Scrollable Stages Example")
 
-canvas = tk.Canvas(root)
-canvas.pack(side="left", fill="both", expand=True)
+sf = ScrolledFrame(root)
+sf.pack(fill='both', expand=True)
 
-scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
-scrollbar.pack(side="right", fill="y")
-
-canvas.configure(yscrollcommand=scrollbar.set)
-
-frame = ttk.Frame(canvas)
-canvas.create_window((0, 0), window=frame, anchor="nw")
-
-inner_frame = ttk.Frame(frame)
-inner_frame.grid(row=0, column=0)
-canvas.create_window((0, 0), window=inner_frame, anchor="nw")
+inner_frame = sf.interior()
 
 stage_count = 0
 
