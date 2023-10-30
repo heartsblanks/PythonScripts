eedeab462211707adce6335965465ffa17cab339
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-30 10:17:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import subprocess
 import tkinter as tk
-import threading
+from tkinter import ttk
 
-def execute_maven_command():
-    command = "mvn install"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    for line in process.stdout:
-        text_box.insert(tk.END, line)
-        text_box.see(tk.END)
-        root.update_idletasks()
-    process.wait()
+def add_stage():
+    global stage_count
+    if stage_count < 100:  # Adjust the limit as needed
+        stage_label = tk.Label(inner_frame, text=f"Stage {stage_count + 1}")
+        stage_label.grid(row=stage_count, column=0, sticky='w')
+        stage_count += 1
+        update_canvas_scrollregion()
 
-def display_output():
-    thread = threading.Thread(target=execute_maven_command)
-    thread.start()
+def update_canvas_scrollregion():
+    canvas.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
 
 root = tk.Tk()
-text_box = tk.Text(root)
-text_box.pack()
+root.title("Scrollable Stages Example")
 
-tk.Button(root, text="Run Maven Command", command=display_output).pack()
+canvas = tk.Canvas(root)
+canvas.pack(side="left", fill="both", expand=True)
+
+scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollbar.pack(side="right", fill="y")
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+frame = ttk.Frame(canvas)
+canvas.create_window((0, 0), window=frame, anchor="nw")
+
+inner_frame = ttk.Frame(frame)
+inner_frame.grid(row=0, column=0)
+canvas.create_window((0, 0), window=inner_frame, anchor="nw")
+
+stage_count = 0
+
+add_button = tk.Button(root, text="Add Stage", command=add_stage)
+add_button.pack()
 
 root.mainloop()
