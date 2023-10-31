668d605b75a1c34da0581eddd72498bf92aa00d6
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-31 16:30:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ source_connections = {}
 # Analyze connections and calculate node positions
 x_offset = 150  # Horizontal offset between nodes
 y_offset = 100  # Vertical offset for multiple connections
+current_x = 100  # Initial horizontal position
 current_y = 100  # Initial vertical position
 
 for connection in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['connections']:
@@ -21,8 +22,8 @@ for connection in message_flow_data['ecore:EPackage']['eClassifiers']['compositi
     target_node = connection['@targetNode']
 
     if source_node not in node_positions:
-        node_positions[source_node] = (0, current_y)
-        current_y += 100  # Adjust vertical spacing for single connections
+        node_positions[source_node] = (current_x, current_y)
+        current_x += x_offset  # Adjust horizontal spacing for single connections
 
     if target_node not in node_positions:
         x_position = node_positions[source_node][0] + x_offset
@@ -49,14 +50,14 @@ for source_node, connections in source_connections.items():
     same_target_connections = [conn for conn in connections if conn['@targetNode'] == connections[0]['@targetNode']]
 
     if len(same_target_connections) > 1:
-        y_offset_multiplier = 1
+        x_offset_multiplier = 1
         for connection in same_target_connections:
             target_node = connection['@targetNode']
-            new_position = (x_position + x_offset * y_offset_multiplier, y_position)
+            new_position = (x_position + x_offset * x_offset_multiplier, y_position)
             while is_overlapping(new_position, list(node_positions.values())):
                 new_position = (new_position[0], new_position[1] + y_offset)
             node_positions[target_node] = new_position
-            y_offset_multiplier += 1
+            x_offset_multiplier += 1
     else:
         for connection in connections:
             target_node = connection['@targetNode']
