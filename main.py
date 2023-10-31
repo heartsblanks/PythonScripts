11c036e752ca4fee8dce347bcd7170ad42ddcf8f
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-31 15:52:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ with open("your_message_flow.xml", "r") as xml_file:
 # Create a dictionary to store node positions
 node_positions = {}
 
+# Create a dictionary to keep track of connections to each target node
+target_connections = {}
+
 # Analyze connections and calculate node positions
 x_offset = 150  # Horizontal offset between nodes
 y_offset = 100  # Vertical offset for multiple connections
@@ -24,26 +27,31 @@ for connection in message_flow_data['ecore:EPackage']['eClassifiers']['compositi
     if target_node not in node_positions:
         x_position = node_positions[source_node][0] + x_offset
         node_positions[target_node] = (x_position, node_positions[source_node][1])
+        target_connections[target_node] = [connection]
     else:
-        # Check if all connections are to the same target node
-        same_target = all(target_node == conn['@targetNode'] for conn in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['connections'] if conn['@sourceNode'] == source_node)
-        if same_target:
-            x_position = node_positions[target_node][0] + x_offset
-            y_position = node_positions[target_node][1]
+        if target_node in target_connections:
+            target_connections[target_node].append(connection)
         else:
-            existing_targets = [t for t in node_positions if node_positions[t][1] < node_positions[source_node][1]]
-            num_existing_targets = len(existing_targets)
-            if num_existing_targets == 0:
-                x_position = node_positions[source_node][0] + x_offset
-                y_position = node_positions[source_node][1] - y_offset
-            elif num_existing_targets == 1:
-                x_position = node_positions[source_node][0] + x_offset
-                y_position = node_positions[source_node][1] + y_offset
-            else:
-                x_position = node_positions[source_node][0] + (num_existing_targets - 1) * x_offset
-                y_position = node_positions[source_node][1] - y_offset
+            target_connections[target_node] = [connection]
 
-        node_positions[target_node] = (x_position, y_position)
+# Update positions for target nodes with multiple connections
+for target_node, connections in target_connections.items():
+    if len(connections) > 1:
+        source_node = connections[0]['@sourceNode']
+        x_position = node_positions[source_node][0]
+        y_position = node_positions[source_node][1]
+
+        for i, connection in connections:
+            if i == 0:
+                # First connection stays on the right
+                x_position += x_offset
+            elif i == 1:
+                # Second connection goes above
+                y_position += y_offset
+            else:
+                # Additional connections go below
+                y_position -= y_offset
+            node_positions[target_node] = (x_position, y_position)
 
 # Update node locations in the XML
 for node in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['nodes']:
