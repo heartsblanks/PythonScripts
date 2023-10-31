acfd4feeb9d9dd0e9a54998b6531e662d763044c
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-31 13:25:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-import tkinter as tk
-from ttkwidgets import ScrolledFrame
+import networkx as nx
+import xmltodict
 
-def add_stage():
-    global stage_count
-    if stage_count < 100:  # Adjust the limit as needed
-        stage_label = tk.Label(inner_frame, text=f"Stage {stage_count + 1}")
-        stage_label.grid(row=stage_count, column=0, sticky='w')
-        stage_count += 1
+# Parse the XML using xmltodict
+with open("your_message_flow.xml", "r") as xml_file:
+    xml_data = xml_file.read()
+    message_flow_data = xmltodict.parse(xml_data)
 
-root = tk.Tk()
-root.title("Scrollable Stages Example")
+# Create a graph
+G = nx.Graph()
 
-sf = ScrolledFrame(root)
-sf.pack(fill='both', expand=True)
+# Extract nodes and connections
+for node in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['nodes']:
+    node_id = node['@xmi:id']
+    location = node['@location']
+    G.add_node(node_id, location=location)
 
-inner_frame = sf.interior()
+for connection in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['connections']:
+    source_node = connection['@sourceNode']
+    target_node = connection['@targetNode']
+    G.add_edge(source_node, target_node)
 
-stage_count = 0
+# Layout the graph (you might need a custom layout algorithm)
+pos = nx.spring_layout(G)
 
-add_button = tk.Button(root, text="Add Stage", command=add_stage)
-add_button.pack()
+# Update node locations in the XML
+for node in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['nodes']:
+    node_id = node['@xmi:id']
+    x, y = pos[node_id].tolist()
+    node['@location'] = f"{int(x)},{int(y)}"
 
-root.mainloop()
+# Serialize the updated XML
+updated_xml = xmltodict.unparse(message_flow_data, pretty=True)
+
+# Save the updated XML
+with open("updated_message_flow.xml", "w") as updated_xml_file:
+    updated_xml_file.write(updated_xml)
