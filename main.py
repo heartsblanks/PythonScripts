5f1047829ea8a346e70aa863386590f041d5c9ab
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-10-31 14:55:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-import networkx as nx
 import xmltodict
+import matplotlib.pyplot as plt
 
 # Parse the XML using xmltodict
 with open("your_message_flow.xml", "r") as xml_file:
     xml_data = xml_file.read()
     message_flow_data = xmltodict.parse(xml_data)
 
-# Create a graph
-G = nx.Graph()
+# Create a figure and axis for plotting
+fig, ax = plt.subplots()
 
 # Extract nodes and connections
+node_positions = {}
 for node in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['nodes']:
     node_id = node['@xmi:id']
-    location = node['@location']
-    G.add_node(node_id, location=location)
+    location = node['@location'].split(',')
+    x, y = map(float, location)
+    node_positions[node_id] = (x, y)
+    ax.annotate(node_id, (x, y))  # Add node IDs as labels
 
 for connection in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['connections']:
     source_node = connection['@sourceNode']
     target_node = connection['@targetNode']
-    G.add_edge(source_node, target_node)
+    x1, y1 = node_positions[source_node]
+    x2, y2 = node_positions[target_node]
+    ax.plot([x1, x2], [y1, y2], 'b-')  # Draw lines for connections
 
-# Layout the graph (you might need a custom layout algorithm)
-pos = nx.spring_layout(G)
+# Customize the layout as needed
 
-# Update node locations in the XML
-for node in message_flow_data['ecore:EPackage']['eClassifiers']['composition']['nodes']:
-    node_id = node['@xmi:id']
-    x, y = pos[node_id].tolist()
-    node['@location'] = f"{int(x)},{int(y)}"
+plt.axis('off')  # Turn off axes
+plt.show()  # Display the layout
 
-# Serialize the updated XML
-updated_xml = xmltodict.unparse(message_flow_data, pretty=True)
-
-# Save the updated XML
-with open("updated_message_flow.xml", "w") as updated_xml_file:
-    updated_xml_file.write(updated_xml)
+# You can save the figure as an image or update the XML with the new positions
