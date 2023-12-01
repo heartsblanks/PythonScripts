6d00956ca86c76af0c73b3f6da524db30599b257
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-01 15:46:33 +0100

    Update tomer.py

--- tomer.py
+++ tomer.py
@@ -4,34 +4,30 @@ import threading
 import time
 
 def update_timer():
-    current_time = datetime.now() - start_time
-    formatted_time = str(current_time).split(".")[0]
-    timer_label.config(text=f"Time: {formatted_time}")
-    if not stop_event.is_set():
-        root.after(1000, update_timer)  # Schedule the next update after 1000 milliseconds (1 second)
+    while not stop_event.is_set():
+        current_time = datetime.now() - start_time
+        formatted_time = str(current_time).split(".")[0]
+        root.after(1000, lambda: timer_label.config(text=f"Time: {formatted_time}"))
+        time.sleep(1)
 
 def simulate_task():
-    start_timer()  # Start the timer before the loop
+    start_timer()  # Start the timer in a separate thread
     for i in range(1, 11):
-        output_label.config(text=f"Number: {i}")
-        time.sleep(1)
-    stop_timer()  # Stop the timer after the loop
+        root.after(i * 1000, lambda i=i: output_label.config(text=f"Number: {i}"))  # Schedule the number update
+    root.after(10000, stop_timer)  # Schedule stopping the timer after 10 seconds
 
 def start_timer():
-    global start_time, stop_event, timer_thread
+    global start_time, stop_event
     start_time = datetime.now()
     stop_event = threading.Event()
     timer_thread = threading.Thread(target=update_timer)
     timer_thread.start()
 
 def stop_timer():
-    global timer_thread
-    if stop_event is not None:
-        stop_event.set()  # Set the event to stop the timer
-        timer_thread.join()  # Wait for the timer thread to finish
-        current_time = datetime.now() - start_time
-        formatted_time = str(current_time).split(".")[0]
-        timer_label.config(text=f"Total Time taken: {formatted_time}")
+    stop_event.set()  # Set the event to stop the timer
+    current_time = datetime.now() - start_time
+    formatted_time = str(current_time).split(".")[0]
+    timer_label.config(text=f"Total Time taken: {formatted_time}")
 
 # Create the main window
 root = tk.Tk()
@@ -40,7 +36,6 @@ root.title("Task with Timer")
 # Initialize variables
 start_time = None
 stop_event = None
-timer_thread = None
 
 # Create and place widgets
 output_label = tk.Label(root, text="Number: ")
