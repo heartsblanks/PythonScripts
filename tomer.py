63310672b0ebbe19d11a88907a26f1c7aecc2c31
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-01 15:29:51 +0100

    Update tomer.py

--- tomer.py
+++ tomer.py
@@ -1,18 +1,19 @@
-import time
+import tkinter as tk
 from datetime import datetime
 import threading
+import time
 
 def update_timer():
     while not stop_event.is_set():
         current_time = datetime.now() - start_time
         formatted_time = str(current_time).split(".")[0]
-        print(f"Time: {formatted_time}")
+        timer_label.config(text=f"Time: {formatted_time}")
         time.sleep(1)
 
 def simulate_task():
     start_timer()  # Start the timer before the loop
     for i in range(1, 11):
-        print(i)
+        output_label.config(text=f"Number: {i}")
         time.sleep(1)
     stop_timer()  # Stop the timer after the loop
 
@@ -28,10 +29,26 @@ def stop_timer():
     timer_thread.join()  # Wait for the timer thread to finish
     current_time = datetime.now() - start_time
     formatted_time = str(current_time).split(".")[0]
-    print(f"Total Time taken: {formatted_time}")
+    timer_label.config(text=f"Total Time taken: {formatted_time}")
 
-# Simulate the task in the main thread
-simulate_task()
+# Create the main window
+root = tk.Tk()
+root.title("Task with Timer")
 
-# Call update_timer independently (after the task has completed, for example)
-update_timer()
+# Initialize variables
+start_time = None
+stop_event = None
+timer_thread = None
+
+# Create and place widgets
+output_label = tk.Label(root, text="Number: ")
+output_label.pack(pady=10)
+
+timer_label = tk.Label(root, text="Time: 00:00:00")
+timer_label.pack()
+
+start_button = tk.Button(root, text="Start Task", command=simulate_task)
+start_button.pack(pady=10)
+
+# Start the GUI
+root.mainloop()
