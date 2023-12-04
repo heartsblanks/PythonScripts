b52eda877cf97ec2e23f5885fceb0e91131d3a80
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-04 10:12:07 +0100

    Update mystique.py

--- mystique.py
+++ mystique.py
@@ -1,33 +1,29 @@
-import tkinter as tk
-from tkinter import ttk
-import time
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt, QPropertyAnimation
 
-class MystiqueIntro:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Mystique Intro")
-        self.root.geometry("800x600")
+class MystiqueIntro(QWidget):
+    def __init__(self):
+        super().__init__()
 
-        self.label = ttk.Label(root, text="Welcome to the Mystique Experience!", font=("Helvetica", 16))
-        self.label.pack(pady=20)
+        layout = QVBoxLayout(self)
+
+        self.label = QLabel("Welcome to the Mystique Experience!", self)
+        layout.addWidget(self.label)
+
+        self.setGeometry(100, 100, 800, 600)
+        self.show()
 
         self.animate_intro()
 
     def animate_intro(self):
-        self.label.config(text="")
-        mystique_text = "Mystique"
-
-        for char in mystique_text:
-            time.sleep(0.5)  # Adjust the sleep duration for your desired pace
-            self.label.config(text=self.label.cget("text") + char)
-            self.root.update()
-
-        # Add additional animation or transitions as needed
-        # For example, you could add fading effects, image transitions, etc.
-
-        # After the intro animation, you might want to transition to the main application
+        animation = QPropertyAnimation(self.label, b"geometry")
+        animation.setDuration(3000)
+        animation.setStartValue(self.label.geometry())
+        animation.setEndValue(self.label.geometry().adjusted(0, 100, 0, 100))
+        animation.setEasingCurve(Qt.CurveType.InOutQuart)
+        animation.start()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = MystiqueIntro(root)
-    root.mainloop()
+    app = QApplication([])
+    window = MystiqueIntro()
+    app.exec_()
