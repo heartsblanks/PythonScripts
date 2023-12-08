3201a16d103747b4fe67d76ba5eb69dd59535e2e
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-08 15:43:40 +0100

    Update mystique.py

--- mystique.py
+++ mystique.py
@@ -9,80 +9,71 @@ screen.title("MYSTIQUE Intro")
 mystique_turtle = turtle.Turtle()
 mystique_turtle.speed(2)
 mystique_turtle.color("white")
+mystique_turtle.pensize(3)
 mystique_turtle.penup()
 mystique_turtle.hideturtle()
 
-# Function to draw MYSTIQUE Netflix-style intro
-def draw_mystique_netflix_intro():
-    mystique_turtle.goto(-80, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(50, 180)  # Draw curved part of 'M'
+# Function to draw appealing MYSTIQUE intro
+def draw_mystique_appealing_intro():
+    draw_appealing_letter("M", -80, 0, mystique_turtle)
+    draw_appealing_letter("Y", -20, 0, mystique_turtle)
+    draw_appealing_letter("S", 40, 0, mystique_turtle)
+    draw_appealing_letter("T", 100, 0, mystique_turtle)
+    draw_appealing_letter("I", 160, 0, mystique_turtle)
+    draw_appealing_letter("Q", 220, 0, mystique_turtle)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(-30, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.right(180)
-    mystique_turtle.circle(50, -180)  # Draw other curved part of 'M'
+# Function to draw appealing letters
+def draw_appealing_letter(letter, x, y, t):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(20, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.forward(40)  # Draw vertical line for 'Y'
+    if letter == "M":
+        t.left(90)
+        t.forward(80)
+        t.right(135)
+        t.forward(40)
+        t.left(135)
+        t.forward(40)
+        t.right(135)
+        t.forward(80)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(70, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(50, 180)  # Draw curved part of 'S'
+    elif letter == "Y":
+        t.left(90)
+        t.forward(40)
+        t.right(90)
+        t.forward(40)
+        t.backward(80)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(120, -40)
-    mystique_turtle.pendown()
-    mystique_turtle.right(90)
-    mystique_turtle.forward(80)  # Draw horizontal line for 'S'
+    elif letter == "S":
+        t.circle(40, -90)
+        t.right(90)
+        t.circle(40, -90)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(70, -80)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(50, -180)  # Draw other curved part of 'S'
+    elif letter == "T":
+        t.forward(40)
+        t.backward(80)
+        t.left(90)
+        t.forward(40)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(170, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.forward(40)  # Draw vertical line for 'T'
+    elif letter == "I":
+        t.forward(80)
+        t.backward(40)
+        t.left(90)
+        t.forward(40)
+        t.left(90)
+        t.forward(80)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(220, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(20, 180)  # Draw curved part of 'I'
+    elif letter == "Q":
+        t.left(90)
+        t.forward(80)
+        t.right(90)
+        t.circle(40, -180)
+        t.left(90)
+        t.forward(80)
 
-    mystique_turtle.penup()
-    mystique_turtle.goto(240, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.forward(40)  # Draw horizontal line for 'I'
-
-    mystique_turtle.penup()
-    mystique_turtle.goto(260, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(20, -180)  # Draw other curved part of 'I'
-
-    mystique_turtle.penup()
-    mystique_turtle.goto(320, 0)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(50, 180)  # Draw curved part of 'Q'
-
-    mystique_turtle.penup()
-    mystique_turtle.goto(370, -40)
-    mystique_turtle.pendown()
-    mystique_turtle.right(90)
-    mystique_turtle.forward(80)  # Draw horizontal line for 'Q'
-
-    mystique_turtle.penup()
-    mystique_turtle.goto(320, -80)
-    mystique_turtle.pendown()
-    mystique_turtle.circle(50, -180)  # Draw other curved part of 'Q'
-
-# Draw the MYSTIQUE Netflix-style intro
-draw_mystique_netflix_intro()
+# Draw the appealing MYSTIQUE intro
+draw_mystique_appealing_intro()
 
 # Close the turtle graphics window on click
 screen.exitonclick()
