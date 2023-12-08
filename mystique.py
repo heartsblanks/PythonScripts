178e96223a592e358cd9acd4ca4000e38f4fb774
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-08 15:37:38 +0100

    Update mystique.py

--- mystique.py
+++ mystique.py
@@ -1,6 +1,18 @@
 import turtle
-import tkinter as tk
 
+# Set up the turtle screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("MYSTIQUE Intro")
+
+# Create a turtle
+mystique_turtle = turtle.Turtle()
+mystique_turtle.speed(2)
+mystique_turtle.color("white")
+mystique_turtle.penup()
+mystique_turtle.hideturtle()
+
+# Function to draw MYSTIQUE
 def draw_mystique():
     mystique_turtle.goto(-100, 0)
     mystique_turtle.pendown()
@@ -27,31 +39,8 @@ def draw_mystique():
     mystique_turtle.write("Q", font=("Arial", 36, "bold"))
     mystique_turtle.penup()
 
-# Create Tkinter window
-root = tk.Tk()
-root.title("MYSTIQUE Intro")
-
-# Create a frame
-frame = tk.Frame(root)
-frame.pack(expand=True, fill="both")
-
-# Create a Canvas widget for turtle graphics
-canvas = tk.Canvas(frame, width=400, height=200)
-canvas.pack()
-
-# Create a turtle screen with a specified background color
-turtle_screen = turtle.Screen()
-turtle_screen.bgcolor("black")
-
-# Create a turtle
-mystique_turtle = turtle.RawTurtle(canvas)
-mystique_turtle.speed(2)
-mystique_turtle.color("white")
-mystique_turtle.penup()
-mystique_turtle.hideturtle()
-
-# Draw MYSTIQUE on canvas
+# Draw the MYSTIQUE animation
 draw_mystique()
 
 # Close the turtle graphics window on click
-root.mainloop()
+screen.exitonclick()
