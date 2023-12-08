0b7c83d335f697c22b7a5582370657e926b422ed
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-12-08 15:53:10 +0100

    Update mystique.py

--- mystique.py
+++ mystique.py
@@ -2,24 +2,21 @@ from turtle import *
 import time
 
 bgcolor('grey')
-speed(2)
-width(5)
+right(90)
 
-up()
-goto(-40, 40)
-down()
-
-fillcolor('red')
-begin_fill()
-
-# Draw the letter "N"
-forward(200)
-right(150)
-forward(220)
-left(150)
-forward(200)
-
-end_fill()
+pos = [-40, 40]
+for x, y in zip(pos, pos):
+    up()
+    goto(x, y)
+    down()
+    fillcolor('red')
+    begin_fill()
+    for _ in range(2):
+        forward(200)
+        right(90)
+        forward(40)
+        right(90)
+    end_fill()
 
 time.sleep(2)
 bgcolor('black')
